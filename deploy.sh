afc1a37770b7b51dbcdfae27a1f8bd324cdaf377
Johan Nystrom-Persson <jtnystrom@gmail.com>
2013-01-09 18:43:35 +0900

    Renamed GWT module to Toxygates

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm otgviewer.war
+mv war/toxygates.war toxygates.war.1
 cp -r ../Friedrich/bin/friedrich/util war/WEB-INF/classes/friedrich
 cp -r ../Friedrich/bin/friedrich/statistics war/WEB-INF/classes/friedrich
 cp -r ../OTGTool/bin/otg war/WEB-INF/classes
 cd war
-zip -r otgviewer.war *
-scp otgviewer.war johan@sontaran:/opt/apache-tomcat-6.0.35/webapps
-#scp otgviewer.war johan@sontaran:~
+zip -r toxygates.war *
+scp toxygates.war johan@sontaran:/opt/apache-tomcat-6.0.35/webapps
+#scp toxygates.war johan@sontaran:~
 cd ..
 rm -r war/WEB-INF/classes/otg
 
