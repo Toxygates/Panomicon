7d5f4d830be8cbbd2db688d98f5c030069543310
Johan Nystrom-Persson <jtnystrom@gmail.com>
2013-05-01 14:06:27 +0900

    Update deploy scripts

--- deployProd.sh
+++ deployProd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Run deploy.sh first to generate toxygates.war
-scp war/toxygates.war   nibioadmin@targetmine:/opt/apache-tomcat-6.0.35/webapps
+#run deploy.sh first to generate toxygates.war
+scp war/toxygates.war   root@202.241.38.91:/opt/apache-tomcat-6.0.36/webapps
 
