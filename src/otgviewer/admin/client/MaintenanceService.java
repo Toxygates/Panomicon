04bf78c99410ea20f22781562ec28fac05882e54
Johan Nystrom-Persson <jtnystrom@gmail.com>
2015-02-06 16:54:05 +0900

    Remove unnecessary interfaces

--- src/otgviewer/admin/client/MaintenanceService.java
+++
@@ -1,8 +0,0 @@
-package otgviewer.admin.client;
-
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-@RemoteServiceRelativePath("maintenance")
-public interface MaintenanceService extends t.admin.client.MaintenanceService {
-
-}
--- src/otgviewer/admin/client/MaintenanceServiceAsync.java
+++
@@ -1,5 +0,0 @@
-package otgviewer.admin.client;
-
-public interface MaintenanceServiceAsync extends t.admin.client.MaintenanceServiceAsync {
-
-}
