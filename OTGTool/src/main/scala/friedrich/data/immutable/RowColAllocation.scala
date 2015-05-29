fb0a6fbd4f43bfe81caf6d6020771f5c6067b5c0
Johan Nystrom-Persson <jtnystrom@gmail.com>
2015-05-29 14:21:07 +0900

    Remove duplicate license headers from friedrich code (was already GPL)

--- OTGTool/src/main/scala/friedrich/data/immutable/RowColAllocation.scala
+++ OTGTool/src/main/scala/friedrich/data/immutable/RowColAllocation.scala
@@ -1,21 +1,7 @@
-/*
- * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health and Nutrition 
- * (NIBIOHN), Japan.
- *
- * This file is part of Toxygates.
- *
- * Toxygates is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Toxygates is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Toxygates. If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Copyright (C) Gabriel Keeble-Gagnere and Johan Nystrom-Persson 2010-2012.
+ * Part of the Friedrich bioinformatics framework.
+ * Dual GPL/MIT license. Please see the files README and LICENSE for details.
  */
 
 package friedrich.data.immutable
