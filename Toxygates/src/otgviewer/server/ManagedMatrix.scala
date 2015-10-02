1ffc7ed9ece0887e017dc16f2363de9de5b3d69b
Johan Nystrom-Persson <jtnystrom@gmail.com>
2015-10-02 16:55:49 +0900

    Bugfix: inverse transform should not be applied to non-fold values

--- Toxygates/src/otgviewer/server/ManagedMatrix.scala
+++ Toxygates/src/otgviewer/server/ManagedMatrix.scala
@@ -76,6 +76,8 @@ abstract class ManagedMatrixBuilder[E >: Null <: ExprValue](reader: MatrixDBRead
       List(g.toString))
   }
 
+  protected def inverseTransform: Boolean = false
+
   def loadRawData(requestColumns: Seq[Group],
     reader: MatrixDBReader[E], sparseRead: Boolean,
     fullLoad: Boolean)(implicit context: MatrixContext): ManagedMatrix = {
@@ -93,7 +95,7 @@ abstract class ManagedMatrixBuilder[E >: Null <: ExprValue](reader: MatrixDBRead
           toVector.distinct
       val sortedSamples = reader.sortSamples(samples.map(b => Sample(b.id)))
       val data = reader.valuesForSamplesAndProbes(sortedSamples,
-        packedProbes, sparseRead, false, true)
+        packedProbes, sparseRead, false, inverseTransform)
 
       println(g.getUnits()(0).toString())
 
@@ -184,6 +186,8 @@ class FoldBuilder(reader: MatrixDBReader[ExprValue], probes: Seq[String])
   protected def columnsFor(g: Group, sortedBarcodes: Seq[Sample],
     data: Seq[Seq[ExprValue]]): ExprMatrix =
     defaultColumns(g, sortedBarcodes, data)
+
+  override protected def inverseTransform = true
 }
 
 trait TreatedControlBuilder[E >: Null <: ExprValue] {
@@ -275,6 +279,8 @@ class ExtFoldBuilder(val enhancedColumns: Boolean, reader: MatrixDBReader[PExprV
 
   def colNames(g: Group) =
     List(g.toString, g.toString + "(p)")
+
+  override protected def inverseTransform = true
 }
 
 /**
