28f3c7e78ca02ab5d8f4aed8d614a2ed3b2beeb9
Johan Nystrom-Persson <jtnystrom@gmail.com>
2015-11-11 17:05:52 +0900

    Cleanup, remove dead code (sample/column packing and unpacking etc)

--- Toxygates/src/otgviewer/shared/OTGColumn.java
+++ Toxygates/src/otgviewer/shared/OTGColumn.java
@@ -23,11 +23,9 @@ package otgviewer.shared;
 import java.util.Set;
 
 import t.common.shared.DataSchema;
-import t.common.shared.Packable;
 import t.common.shared.sample.DataColumn;
-import t.common.shared.sample.HasSamples;
 
-public interface OTGColumn extends Packable, DataColumn<OTGSample>, HasSamples<OTGSample> {
+public interface OTGColumn extends DataColumn<OTGSample> {
 	/**
 	 * Obtain the set of all compounds that the samples in this column are associated with.
 	 * @return
--- Toxygates/src/otgviewer/shared/OTGSample.java
+++ Toxygates/src/otgviewer/shared/OTGSample.java
@@ -20,9 +20,6 @@
 
 package otgviewer.shared;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.annotation.Nullable;
 
 import t.common.shared.DataSchema;
@@ -50,66 +47,18 @@ public class OTGSample extends Sample {
 	public String toString() {
 		return sampleClass.toString();
 	}
-
-	//TODO remove or upgrade to use DataSchema 
-	public static OTGSample unpack_v1_2(String s) {
-//		Window.alert(s + " as barcode");
-		String[] s1 = s.split("\\$\\$\\$");
-		
-		Map<String, String> sc = new HashMap<String, String>();
-		String id = s1[1];
-		sc.put("individual_id", s1[2]);
-		sc.put("dose_level", s1[3]);
-		sc.put("exposure_time", s1[4]);
-		sc.put("compound_name", s1[5]);
-		
-		if (s1.length == 6) {		
-			//Version 1
-			return new OTGSample(id, new SampleClass(sc), null);
-		} else if (s1.length == 10) {
-			//Version 2
-			sc.put("test_type", s1[6]);
-			sc.put("organ_id", s1[7]);
-			sc.put("sin_rep_type", s1[8]);
-			sc.put("organism", s1[9]);
-			return new OTGSample(id, new SampleClass(sc), null);
-		} else {			
-			return null;
-		}
-	}
 	
 	public static OTGSample unpack(String s) {
 		String[] spl = s.split("\\$\\$\\$");
 		String v = spl[0];
 		if (!v.equals("Barcode_v3")) {
-			return unpack_v1_2(s);
+		  //Incorrect/legacy format - TODO: warn here
+		   return null;
 		} 
 		String id = spl[1];
 		SampleClass sc = SampleClass.unpack(spl[2]);
-		return new OTGSample(id, sc, null);
-		
+		return new OTGSample(id, sc, null);		
 	}
-	
-	//V 1/2 packing function
-//	
-//	public String pack() {
-//		final String sep = "$$$";
-//		StringBuilder sb = new StringBuilder();
-//		sb.append("Barcode").append(sep);
-//		sb.append(id()).append(sep);
-//		sb.append(individual).append(sep);
-//		sb.append(dose).append(sep);
-//		sb.append(time).append(sep);
-//		sb.append(compound).append(sep);
-//		
-//		if (sampleClass.get("test_type") != null) {
-//			sb.append(sampleClass.get("test_type")).append(sep);
-//			sb.append(sampleClass.get("organ_id")).append(sep);
-//			sb.append(sampleClass.get("sin_rep_type")).append(sep);
-//			sb.append(sampleClass.get("organism")).append(sep);
-//		}
-//		return sb.toString();
-//	}
 	
 	public String pack() {
 		final String sep = "$$$";
