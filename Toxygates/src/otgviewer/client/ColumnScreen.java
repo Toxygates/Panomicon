2925f283e47db2a9e3248959d93d3f30922f4969
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-01-04 15:39:23 +0900

    Formatting

--- Toxygates/src/otgviewer/client/ColumnScreen.java
+++ Toxygates/src/otgviewer/client/ColumnScreen.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health and Nutrition 
- * (NIBIOHN), Japan.
- *
+ * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health
+ * and Nutrition (NIBIOHN), Japan.
+ * 
  * This file is part of Toxygates.
- *
- * Toxygates is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Toxygates is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Toxygates. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Toxygates is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * Toxygates is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Toxygates. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 
 package otgviewer.client;
@@ -47,125 +45,127 @@ import com.google.gwt.user.client.ui.Widget;
  * This screen allows for column (group) definition as well as compound ranking.
  */
 public class ColumnScreen extends Screen {
-	public static String key = "columns";
-	
-	private GroupInspector gi;
-	private CompoundSelector cs;
-	private FilterTools filterTools;
+  public static String key = "columns";
 
-	public ColumnScreen(ScreenManager man) {
-		super("Sample groups", key, false, man,
-				resources.groupDefinitionHTML(), resources.groupDefinitionHelp());
-		
-		String majorParam = man.schema().majorParameter();
-		cs = new CompoundSelector(this, man.schema().title(majorParam), true, true) {
-		  @Override
-	      public void changeCompounds(List<String> compounds) {
-	        super.changeCompounds(compounds);
-	        storeCompounds(getParser(ColumnScreen.this));
-	      }  
-		};		
-		this.addListener(cs);
-		cs.setStylePrimaryName("compoundSelector");
-		
-	    chosenDatasets = appInfo().datasets();
-		filterTools = new FilterTools(this); 	
-		this.addListener(filterTools);
-	} 
-	
-	@Override
-	protected void addToolbars() {
-		super.addToolbars();
-		addToolbar(filterTools, 45);
-		addLeftbar(cs, 350);
-	}
-	
-	@Override
-	protected boolean shouldShowStatusBar() {
-		return false;
-	}
+  private GroupInspector gi;
+  private CompoundSelector cs;
+  private FilterTools filterTools;
 
-	public Widget content() {				
-		gi = factory().groupInspector(cs, this);
-		this.addListener(gi);
-		cs.addListener(gi);
-		gi.datasetsChanged(chosenDatasets);
-		gi.addStaticGroups(appInfo().predefinedSampleGroups());
-		return gi;
-	}
+  public ColumnScreen(ScreenManager man) {
+    super("Sample groups", key, false, man, resources.groupDefinitionHTML(), resources
+        .groupDefinitionHelp());
 
-	@Override
-	public Widget bottomContent() {
-		HorizontalPanel hp = Utils.mkWidePanel();
-		
-		Button b = new Button("Delete all groups", new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				gi.confirmDeleteAllGroups();				
-			}			
-		});		
-		
-		Button b2 = new Button("Next: View data", new ClickHandler() {			
-			public void onClick(ClickEvent event) {
-				if (gi.chosenColumns().size() == 0) {
-					Window.alert("Please define and activate at least one group.");
-				} else {
-					configuredProceed(DataScreen.key);					
-				}
-			}
-		});
-		
-		hp.add(Utils.mkHorizontalPanel(true, b, b2));
-		return hp;
-	}
-	
-	@Override
-	public void loadState(StorageParser p, DataSchema schema) {
-		super.loadState(p, schema);
-		if (visible) {
-			try {
-				List<Group> ics = loadColumns(p, schema(), "inactiveColumns", 
-						new ArrayList<SampleColumn>(gi.existingGroupsTable().inverseSelection()));
-				if (ics != null && ics.size() > 0) {
-					logger.info("Unpacked i. columns: " + ics.get(0) + ": " + ics.get(0).getSamples()[0] + " ... ");
-					gi.inactiveColumnsChanged(ics);
-				} else {
-					logger.info("No i. columns available");
-				}
+    String majorParam = man.schema().majorParameter();
+    cs = new CompoundSelector(this, man.schema().title(majorParam), true, true) {
+      @Override
+      public void changeCompounds(List<String> compounds) {
+        super.changeCompounds(compounds);
+        storeCompounds(getParser(ColumnScreen.this));
+      }
+    };
+    this.addListener(cs);
+    cs.setStylePrimaryName("compoundSelector");
 
-			} catch (Exception e) {
-				logger.log(Level.WARNING, "Unable to load i. columns", e);
-				Window.alert("Unable to load inactive columns.");
-			}
-		}
-	}
-	
-	@Override
-	public void changeSampleClass(SampleClass sc) {
-		//On this screen, ignore the blank sample class set by
-		//DataListenerWidget
-		if (!sc.getMap().isEmpty()) {
-			super.changeSampleClass(sc);
-			storeSampleClass(getParser());
-		}
-	}
+    chosenDatasets = appInfo().datasets();
+    filterTools = new FilterTools(this);
+    this.addListener(filterTools);
+  }
 
-	@Override
-	public void tryConfigure() {
-		if (chosenColumns.size() > 0) {		
-			setConfigured(true);
-		}
-	}
+  @Override
+  protected void addToolbars() {
+    super.addToolbars();
+    addToolbar(filterTools, 45);
+    addLeftbar(cs, 350);
+  }
 
-	@Override
-	public void resizeInterface() {
-		//Test carefully in IE8, IE9 and all other browsers if changing this method
-		cs.resizeInterface();
-		super.resizeInterface();		
-	}
+  @Override
+  protected boolean shouldShowStatusBar() {
+    return false;
+  }
 
-	@Override
-	public String getGuideText() {
-		return "Please define at least one sample group to proceed. Start by selecting compounds to the left. Then select doses and times.";
-	}
+  public Widget content() {
+    gi = factory().groupInspector(cs, this);
+    this.addListener(gi);
+    cs.addListener(gi);
+    gi.datasetsChanged(chosenDatasets);
+    gi.addStaticGroups(appInfo().predefinedSampleGroups());
+    return gi;
+  }
+
+  @Override
+  public Widget bottomContent() {
+    HorizontalPanel hp = Utils.mkWidePanel();
+
+    Button b = new Button("Delete all groups", new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        gi.confirmDeleteAllGroups();
+      }
+    });
+
+    Button b2 = new Button("Next: View data", new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        if (gi.chosenColumns().size() == 0) {
+          Window.alert("Please define and activate at least one group.");
+        } else {
+          configuredProceed(DataScreen.key);
+        }
+      }
+    });
+
+    hp.add(Utils.mkHorizontalPanel(true, b, b2));
+    return hp;
+  }
+
+  @Override
+  public void loadState(StorageParser p, DataSchema schema) {
+    super.loadState(p, schema);
+    if (visible) {
+      try {
+        List<Group> ics =
+            loadColumns(p, schema(), "inactiveColumns", new ArrayList<SampleColumn>(gi
+                .existingGroupsTable().inverseSelection()));
+        if (ics != null && ics.size() > 0) {
+          logger.info("Unpacked i. columns: " + ics.get(0) + ": " + ics.get(0).getSamples()[0]
+              + " ... ");
+          gi.inactiveColumnsChanged(ics);
+        } else {
+          logger.info("No i. columns available");
+        }
+
+      } catch (Exception e) {
+        logger.log(Level.WARNING, "Unable to load i. columns", e);
+        Window.alert("Unable to load inactive columns.");
+      }
+    }
+  }
+
+  @Override
+  public void changeSampleClass(SampleClass sc) {
+    // On this screen, ignore the blank sample class set by
+    // DataListenerWidget
+    if (!sc.getMap().isEmpty()) {
+      super.changeSampleClass(sc);
+      storeSampleClass(getParser());
+    }
+  }
+
+  @Override
+  public void tryConfigure() {
+    if (chosenColumns.size() > 0) {
+      setConfigured(true);
+    }
+  }
+
+  @Override
+  public void resizeInterface() {
+    // Test carefully in IE8, IE9 and all other browsers if changing this method
+    cs.resizeInterface();
+    super.resizeInterface();
+  }
+
+  @Override
+  public String getGuideText() {
+    return "Please define at least one sample group to proceed. Start by selecting compounds to the left. Then select doses and times.";
+  }
 }
--- Toxygates/src/otgviewer/client/RankingScreen.java
+++ Toxygates/src/otgviewer/client/RankingScreen.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health and Nutrition 
- * (NIBIOHN), Japan.
- *
+ * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health
+ * and Nutrition (NIBIOHN), Japan.
+ * 
  * This file is part of Toxygates.
- *
- * Toxygates is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Toxygates is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Toxygates. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Toxygates is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * Toxygates is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Toxygates. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 
 package otgviewer.client;
@@ -37,26 +35,25 @@ import com.google.gwt.user.client.ui.Widget;
 public class RankingScreen extends Screen {
 
   public static final String key = "rank";
-  
+
   private RankingCompoundSelector cs;
   private FilterTools filterTools;
   private ScrollPanel sp;
-  
+
   public RankingScreen(ScreenManager man) {
-    super("Compound ranking", key, false, man,
-        resources.defaultHelpHTML(), null);
-    
+    super("Compound ranking", key, false, man, resources.defaultHelpHTML(), null);
+
     chosenDatasets = appInfo().datasets();
     filterTools = new FilterTools(this);
-    this.addListener(filterTools);    
-    
+    this.addListener(filterTools);
+
     String majorParam = man.schema().majorParameter();
     cs = new RankingCompoundSelector(this, man.schema().title(majorParam)) {
       @Override
       public void changeCompounds(List<String> compounds) {
         super.changeCompounds(compounds);
         storeCompounds(getParser(RankingScreen.this));
-      }      
+      }
     };
     this.addListener(cs);
     cs.setStylePrimaryName("compoundSelector");
@@ -64,46 +61,46 @@ public class RankingScreen extends Screen {
 
   @Override
   protected void addToolbars() {
-      super.addToolbars();
-      addToolbar(filterTools, 45);
-      addLeftbar(cs, 350);
+    super.addToolbars();
+    addToolbar(filterTools, 45);
+    addLeftbar(cs, 350);
   }
-  
+
   public Widget content() {
     CompoundRanker cr = factory().compoundRanker(this, cs);
     sp = makeScrolled(cr);
     return sp;
   }
-  
+
   @Override
   protected boolean shouldShowStatusBar() {
     return false;
   }
-  
+
   @Override
   public void changeSampleClass(SampleClass sc) {
-      //On this screen, ignore the blank sample class set by
-      //DataListenerWidget
-      if (!sc.getMap().isEmpty()) {
-          super.changeSampleClass(sc);
-          storeSampleClass(getParser());
-      }      
+    // On this screen, ignore the blank sample class set by
+    // DataListenerWidget
+    if (!sc.getMap().isEmpty()) {
+      super.changeSampleClass(sc);
+      storeSampleClass(getParser());
+    }
   }
-  
+
   @Override
   public void resizeInterface() {
-      //Test carefully in IE8, IE9 and all other browsers if changing this method
-      cs.resizeInterface();     
-      super.resizeInterface();        
+    // Test carefully in IE8, IE9 and all other browsers if changing this method
+    cs.resizeInterface();
+    super.resizeInterface();
   }
-  
+
   public void show() {
     super.show();
   }
-  
+
   @Override
   public String getGuideText() {
-      return "Specify at least one gene symbol to rank compounds according to their effect.";
+    return "Specify at least one gene symbol to rank compounds according to their effect.";
   }
 
 }
