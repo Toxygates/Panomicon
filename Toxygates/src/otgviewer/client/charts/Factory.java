faf11f828afbf6a3da7ab4a42a9aaa1b7456be2c
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-01-08 16:07:17 +0900

    Source formatting for otgviewer.client.charts and otgviewer.client.charts.google

--- Toxygates/src/otgviewer/client/charts/Factory.java
+++ Toxygates/src/otgviewer/client/charts/Factory.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health and Nutrition 
- * (NIBIOHN), Japan.
- *
+ * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health
+ * and Nutrition (NIBIOHN), Japan.
+ * 
  * This file is part of Toxygates.
- *
- * Toxygates is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Toxygates is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Toxygates. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Toxygates is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * Toxygates is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Toxygates. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 
 package otgviewer.client.charts;
@@ -26,17 +24,14 @@ import otgviewer.client.components.Screen;
 
 abstract public class Factory<D extends Data, DS extends Dataset<D>> {
 
-	abstract public D[][] dataArray(int rows, int cols);
+  abstract public D[][] dataArray(int rows, int cols);
 
-	abstract public DS dataset(List<ChartSample> samples, 
-			List<ChartSample> allSamples, String[] categories,
-			boolean categoriesAreMins);
-	
-	abstract public ChartGrid<D> grid(Screen screen, DS table,  
-			final List<String> rowFilters, final List<String> organisms,
-			boolean rowsAreMajors, 
-			String[] timesOrDoses, 			
-			boolean columnsAreTimes, int totalWidth);
-	
-	abstract public int gridMaxWidth();
+  abstract public DS dataset(List<ChartSample> samples, List<ChartSample> allSamples,
+      String[] categories, boolean categoriesAreMins);
+
+  abstract public ChartGrid<D> grid(Screen screen, DS table, final List<String> rowFilters,
+      final List<String> organisms, boolean rowsAreMajors, String[] timesOrDoses,
+      boolean columnsAreTimes, int totalWidth);
+
+  abstract public int gridMaxWidth();
 }
--- Toxygates/src/otgviewer/client/charts/google/GVizFactory.java
+++ Toxygates/src/otgviewer/client/charts/google/GVizFactory.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health and Nutrition 
- * (NIBIOHN), Japan.
- *
+ * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health
+ * and Nutrition (NIBIOHN), Japan.
+ * 
  * This file is part of Toxygates.
- *
- * Toxygates is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Toxygates is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Toxygates. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Toxygates is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * Toxygates is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Toxygates. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 
 package otgviewer.client.charts.google;
@@ -32,36 +30,33 @@ import t.common.shared.sample.Group;
 
 public class GVizFactory extends Factory<GDTData, GDTDataset> {
 
-	@Override
-	public GDTData[][] dataArray(int rows, int cols) {
-		return new GDTData[rows][cols];
-	}
+  @Override
+  public GDTData[][] dataArray(int rows, int cols) {
+    return new GDTData[rows][cols];
+  }
 
-	@Override
-	public GDTDataset dataset(List<ChartSample> samples,
-			List<ChartSample> allSamples, String[] categories,
-			boolean categoriesAreMins) {
-		return new GDTDataset(samples, allSamples, categories,
-				categoriesAreMins);
-	}
+  @Override
+  public GDTDataset dataset(List<ChartSample> samples, List<ChartSample> allSamples,
+      String[] categories, boolean categoriesAreMins) {
+    return new GDTDataset(samples, allSamples, categories, categoriesAreMins);
+  }
 
-	@Override
-	public GVizChartGrid grid(Screen screen, GDTDataset table, List<String> rowFilters,
-			List<String> organisms, boolean rowsAreMajors,
-			String[] timesOrDoses, boolean columnsAreTimes, int totalWidth) {
-		return new GVizChartGrid(this, screen, table, rowFilters,
-				organisms, rowsAreMajors, timesOrDoses, columnsAreTimes,
-				totalWidth);
-	}
+  @Override
+  public GVizChartGrid grid(Screen screen, GDTDataset table, List<String> rowFilters,
+      List<String> organisms, boolean rowsAreMajors, String[] timesOrDoses,
+      boolean columnsAreTimes, int totalWidth) {
+    return new GVizChartGrid(this, screen, table, rowFilters, organisms, rowsAreMajors,
+        timesOrDoses, columnsAreTimes, totalWidth);
+  }
 
-	public AdjustableGrid<GDTData, GDTDataset> adjustableGrid(Screen screen, 
-			DataSource source, List<Group> groups, ValueType vt) {
-		return new AdjustableGrid<GDTData, GDTDataset>(this, screen, source, groups, vt);
-	}
-	
-	@Override
-	public int gridMaxWidth() {
-		return GVizChartGrid.MAX_WIDTH;
-	}
+  public AdjustableGrid<GDTData, GDTDataset> adjustableGrid(Screen screen, DataSource source,
+      List<Group> groups, ValueType vt) {
+    return new AdjustableGrid<GDTData, GDTDataset>(this, screen, source, groups, vt);
+  }
+
+  @Override
+  public int gridMaxWidth() {
+    return GVizChartGrid.MAX_WIDTH;
+  }
 
 }
