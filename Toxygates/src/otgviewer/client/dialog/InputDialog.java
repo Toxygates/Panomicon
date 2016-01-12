8ab610f1b7a29016e7b67c5a5061eb1c6803de54
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-01-12 15:34:29 +0900

    Source formatting for components.groupdef, components.ranking, components.dialog

--- Toxygates/src/otgviewer/client/dialog/InputDialog.java
+++ Toxygates/src/otgviewer/client/dialog/InputDialog.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health and Nutrition 
- * (NIBIOHN), Japan.
- *
+ * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health
+ * and Nutrition (NIBIOHN), Japan.
+ * 
  * This file is part of Toxygates.
- *
- * Toxygates is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Toxygates is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Toxygates. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Toxygates is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * Toxygates is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Toxygates. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 
 package otgviewer.client.dialog;
@@ -35,40 +33,40 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class InputDialog extends Composite {
 
-	public InputDialog(String message) {
-		VerticalPanel vp = new VerticalPanel();
-		vp.setWidth("100%");
-		vp.add(new Label(message));
-		initWidget(vp);
-		
-		final TextBox input = new TextBox();
-		vp.add(input);
-		
-		Button b = new Button("OK");
-		b.addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				onChange(input.getText());
-				
-			}
-		});
-		
-		Button b2 = new Button("Cancel");
-		b2.addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				onChange(null);			
-			}
-		});		
-		HorizontalPanel hp = Utils.mkHorizontalPanel(true, b, b2);
-		hp.setWidth("100%");				
-		vp.add(hp);
-	}
-	
-	protected void onChange(@Nullable String result) {
-		
-	}
+  public InputDialog(String message) {
+    VerticalPanel vp = new VerticalPanel();
+    vp.setWidth("100%");
+    vp.add(new Label(message));
+    initWidget(vp);
+
+    final TextBox input = new TextBox();
+    vp.add(input);
+
+    Button b = new Button("OK");
+    b.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(ClickEvent event) {
+        onChange(input.getText());
+
+      }
+    });
+
+    Button b2 = new Button("Cancel");
+    b2.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(ClickEvent event) {
+        onChange(null);
+      }
+    });
+    HorizontalPanel hp = Utils.mkHorizontalPanel(true, b, b2);
+    hp.setWidth("100%");
+    vp.add(hp);
+  }
+
+  protected void onChange(@Nullable String result) {
+
+  }
 
 }
--- Toxygates/src/otgviewer/client/dialog/MetadataInfo.java
+++ Toxygates/src/otgviewer/client/dialog/MetadataInfo.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health and Nutrition
- * (NIBIOHN), Japan.
- *
+ * Copyright (c) 2012-2015 Toxygates authors, National Institutes of Biomedical Innovation, Health
+ * and Nutrition (NIBIOHN), Japan.
+ * 
  * This file is part of Toxygates.
- *
- * Toxygates is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Toxygates is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Toxygates. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Toxygates is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * Toxygates is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Toxygates. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 
 package otgviewer.client.dialog;
@@ -33,45 +31,45 @@ public class MetadataInfo extends Composite {
   public static MetadataInfo fromPlatforms(Platform[] platforms) {
     String[] titles = new String[platforms.length];
     String[] comments = new String[platforms.length];
-    
+
     for (int i = 0; i < platforms.length; ++i) {
       titles[i] = platforms[i].getTitle();
       comments[i] = platforms[i].getPublicComment();
     }
-    
+
     return new MetadataInfo("Platform", titles, comments);
   }
-  
-  public static MetadataInfo annotations(AppInfo appInfo) {    
+
+  public static MetadataInfo annotations(AppInfo appInfo) {
     return new MetadataInfo("Annotation", appInfo.getAnnotationTitles(),
         appInfo.getAnnotationComments());
   }
 
-  public MetadataInfo(String type, String[] titles, String[] comments) {  
+  public MetadataInfo(String type, String[] titles, String[] comments) {
     VerticalPanel vp = new VerticalPanel();
     initWidget(vp);
-    
+
     Grid g = new Grid(titles.length + 1, 2);
     vp.add(g);
     g.setStylePrimaryName("metadata-grid");
     g.getColumnFormatter().setStyleName(0, "metadata-firstColumn");
     g.getRowFormatter().setStyleName(0, "metadata-firstRow");
-    
+
     Label l = new Label(type);
     l.setStylePrimaryName("metadata-info-heading");
     g.setWidget(0, 0, l);
     l = new Label("Comment");
     l.setStylePrimaryName("metadata-info-heading");
     g.setWidget(0, 1, l);
-    
-    for (int i = 1; i < titles.length + 1; ++ i) {    
-      l = new Label(titles[i-1]);
+
+    for (int i = 1; i < titles.length + 1; ++i) {
+      l = new Label(titles[i - 1]);
       l.setStylePrimaryName("metadata-info-title");
       g.setWidget(i, 0, l);
-      
-      l = new Label(comments[i-1]);
+
+      l = new Label(comments[i - 1]);
       l.setStylePrimaryName("metadata-info-comment");
       g.setWidget(i, 1, l);
-    }  
+    }
   }
 }
