a2adcfc878ad92bc66f73ce2dc4e08eae0daf666
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-04-28 12:46:42 +0900

    Missing file for last commit

---
+++ Toxygates/src/t/common/shared/GroupedDataset.java
@@ -0,0 +1,26 @@
+package t.common.shared;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This class is intended mainly for grouping shared datasets, which will have
+ * the same user title but different IDs.
+ * @author johan
+ *
+ */
+public class GroupedDataset extends Dataset {
+
+  private List<Dataset> subDatasets;
+  
+  public GroupedDataset(String title, String description, List<Dataset> subDatasets) {
+    super(title, description, "", new Date(), "");
+    this.subDatasets = subDatasets;
+  }
+  
+  @Override
+  public Collection<Dataset> getSubDatasets() {
+    return subDatasets;    
+  }
+}
