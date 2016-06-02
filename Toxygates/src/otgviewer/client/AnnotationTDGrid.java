59cd4096a4b2988939949aabe77ab47350ef239e
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-06-02 14:59:29 +0900

    Remove unused imports

--- Toxygates/src/otgviewer/client/AnnotationTDGrid.java
+++ Toxygates/src/otgviewer/client/AnnotationTDGrid.java
@@ -31,8 +31,6 @@ import t.common.shared.sample.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.NumberFormat;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
