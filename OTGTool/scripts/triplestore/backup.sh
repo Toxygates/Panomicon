e2f7dea9dce45e88662de25434c0f9e79dba4c27
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-06-03 15:29:39 +0900

    Keep backups for longer

--- OTGTool/scripts/triplestore/backup.sh
+++ OTGTool/scripts/triplestore/backup.sh
@@ -3,26 +3,35 @@
 BACKUPDIR=/home/nibioadmin/toxygates_backup
 
 function rdf_backup {
+ cd $BACKUPDIR
+
  REPO=$1
- curl -u xyzuser:xyzpass -X GET -H "Accept:application/x-trig"   "http://sontaran:8081/owlim-workbench-webapp-5.3.1/repositories/$REPO/rdf-graphs/service?graph=http://www.ontotext.com/explicit" | gzip -f > $BACKUPDIR/$REPO.trig.gz
+ [ -f $REPO.trig.1.gz ] && rm -f $REPO.trig.1.gz
+ [ -f $REPO.trig.gz ] && mv $REPO.trig.gz $REPO.trig.1.gz
+
+ curl -u user:pass -X GET -H "Accept:application/x-trig"   "http://sontaran:8081/owlim-workbench-webapp-5.3.1/repositories/$REPO/rdf-graphs/service?graph=http://www.ontotext.com/explicit" | gzip -f > $REPO.trig.gz
 }
 
 #RDF backup
-rdf_backup otg
+#rdf_backup otg
 rdf_backup ttest
 
 #KC backup
-#mkdir -p $BACKUPDIR/data-main
-#mkdir -p $BACKUPDIR/data-adjuvant
-mkdir -p $BACKUPDIR/data-dev
 
 function backup {
  SOURCE=$1
  DEST=$2
+ [ -d $DEST.1 ] && rm -rf $DEST.1
+ [ -d $DEST ] && mv $DEST $DEST.1
+
+ mkdir -p $DEST
  scp nibioadmin@toxygates:$SOURCE/*.kct $DEST
+ scp nibioadmin@toxygates:$SOURCE/*.kch $DEST
  gzip -f $DEST/*.kct
+ gzip -f $DEST/*.kch
 }
 
 #backup /opt/toxygates $BACKUPDIR/data-main
-backup /opt/toxygates-dev $BACKUPDIR/data-dev
+#backup /opt/toxygates-dev $BACKUPDIR/data-dev
+backup /opt/toxygates-chunk $BACKUPDIR/data-chunk
 
