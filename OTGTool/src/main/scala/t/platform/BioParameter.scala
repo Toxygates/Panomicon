a1a4ad2604a3de271a994c922c98203ce12ef0f1
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-12-22 15:12:55 +0900

    Different BioParameter upper and lower bounds for different time points - work in progress. Also break out annotation code from SparqlServiceImpl

--- OTGTool/src/main/scala/t/platform/BioParameter.scala
+++ OTGTool/src/main/scala/t/platform/BioParameter.scala
@@ -4,7 +4,8 @@ import t.db.SampleParameter
 
 case class BioParameter(key: String, label: String, kind: String,
     section: Option[String],
-    lowThreshold: Option[Double], highThreshold: Option[Double]) {
+    lowerBound: Option[Double], upperBound: Option[Double],
+    attributes: Map[String, String] = Map()) {
 
   /**
    * Obtain an equivalent SampleParameter (for lookup purposes)
@@ -17,4 +18,25 @@ class BioParameters(lookup: Map[String, BioParameter]) {
   def get(key: String) = lookup.get(key)
 
   def sampleParameters = lookup.values.map(_.sampleParameter)
+
+  /**
+   * Extract bio parameters with accurate low and high threshold for a given
+   * time point. The raw values are stored in the root parameter set's
+   * attribute maps.
+   * @param time The time point, e.g. "24 hr"
+   */
+  def forTimePoint(time: String): BioParameters = {
+    val normalTime = time.replaceAll("\\s+", "")
+
+    new BioParameters(Map() ++
+      (for (
+        (id, param) <- lookup;
+        lb = param.attributes.get(s"lowerBound_$normalTime").
+          map(_.toDouble).orElse(param.lowerBound);
+        ub = param.attributes.get(s"upperBound_$normalTime").
+          map(_.toDouble).orElse(param.upperBound);
+        edited = BioParameter(id, param.label, param.kind, param.section, lb, ub,
+          param.attributes)
+      ) yield (id -> edited)))
+  }
 }
