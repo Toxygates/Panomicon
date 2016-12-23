729c1ac8b6c17f95a07070fbfcaf94d1cb1a58e4
Johan Nystrom-Persson <jtnystrom@gmail.com>
2016-12-23 12:57:13 +0900

    Tool to calculate variance and std. dev of biological parameters

--- OTGTool/src/main/scala/t/platform/BioParameter.scala
+++ OTGTool/src/main/scala/t/platform/BioParameter.scala
@@ -1,6 +1,8 @@
 package t.platform
 
 import t.db.SampleParameter
+import t.db.file.TSVMetadata
+import t.db.file.MapMetadata
 
 case class BioParameter(key: String, label: String, kind: String,
     section: Option[String],
@@ -38,5 +40,51 @@ class BioParameters(lookup: Map[String, BioParameter]) {
         edited = BioParameter(id, param.label, param.kind, param.section, lb, ub,
           param.attributes)
       ) yield (id -> edited)))
+  }
+}
+
+object BioParameter {
+  import org.apache.commons.math3.stat.StatUtils.variance
+  import org.apache.commons.math3.stat.StatUtils.mean
+
+  def convert(x: String) = x match {
+      case "NA" => None
+      case _    => Some(x.toDouble)
+    }
+
+  def main(args: Array[String]) {
+     val f = new otg.Factory
+     val params = otg.db.OTGParameterSet
+     val data = TSVMetadata(f, args(0), params)
+     var out = Map[SampleParameter, Seq[String]]()
+
+     for (time <- data.parameterValues("exposure_time")) {
+       val ftime = time.replaceAll("\\s+", "")
+       var samples = data.samples
+       samples = samples.filter(s => {
+         val m = data.parameterMap(s)
+         m("exposure_time") == time && m("dose_level") == "Control" && m("test_type") == "in vivo"
+       })
+       val rawValues = samples.map(s => data.parameters(s))
+       for (param <- params.all; if params.isNumerical(param)) {
+         if (!out.contains(param)) {
+           out += param -> Seq()
+         }
+
+        val rawdata = rawValues.map(_.find( _._1 == param).get).map(x => convert(x._2))
+        if (!rawdata.isEmpty) {
+          val v = variance(rawdata.flatten.toArray)
+          val m = mean(rawdata.flatten.toArray)
+          val sd = Math.sqrt(v)
+          val upper = m + 2 * sd
+          val lower = m - 2 * sd
+          out += param -> (out(param) :+ s"lowerBound_$ftime=$lower")
+          out += param -> (out(param) :+ s"upperBound_$ftime=$upper")
+        }
+       }
+     }
+     for ((k, vs) <- out) {
+       println(k.identifier + "\t" + vs.mkString(","))
+     }
   }
 }
