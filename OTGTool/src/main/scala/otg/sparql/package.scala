d8c8e0d0af289f5adbc05a3cb24f3e79b485faef
Johan Nystrom-Persson <jtnystrom@gmail.com>
2017-01-19 18:29:54 +0900

    Clean up OTG Sample queries, use string filtering - work in progress

--- OTGTool/src/main/scala/otg/sparql/package.scala
+++ OTGTool/src/main/scala/otg/sparql/package.scala
@@ -27,11 +27,11 @@ import t.sparql.QueryUtils
 package object sparql extends t.sparql.QueryUtils {
   import Triplestore._
 
-  val commonPrefixes = tPrefixes + """
-    PREFIX local:<http://127.0.0.1:3333/>
-    PREFIX luc: <http://www.ontotext.com/owlim/lucene#>
-    PREFIX bio2rdf:<http://bio2rdf.org/ns/bio2rdf#>
-"""
+  //TODO update pathology sparql and retire the local prefix
+  val commonPrefixes = s"""$tPrefixes
+    |PREFIX local:<http://127.0.0.1:3333/>
+    |PREFIX luc: <http://www.ontotext.com/owlim/lucene#>
+    |PREFIX bio2rdf:<http://bio2rdf.org/ns/bio2rdf#>""".stripMargin
 
   def infixStringMatch(q: String) = " luc:myIndex \"*" + q + "*\". "
   def prefixStringMatch(q: String) = " luc:myIndex \"" + q + "*\". "
