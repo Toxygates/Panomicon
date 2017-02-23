40db29c11371f74ec6056db4d39d3027927b902a
Johan Nystrom-Persson <jtnystrom@gmail.com>
2017-02-23 16:58:16 +0900

    More efficient update_annotations with background processes

--- OTGTool/scripts/triplestore/update_annotations.sh
+++ OTGTool/scripts/triplestore/update_annotations.sh
@@ -17,10 +17,12 @@ ssh toxygates touch $MAINTENANCE_FILE
 
 curl -O http://geneontology.org/ontology/go.owl
 $TGHOME/replace.sh $AHOME/go.owl $REPO http://level-five.jp/t/annotation/go \
-	"GO terms" "Updated $(date) from go.owl"
+        "GO terms" "Updated $(date) from go.owl" &
 
-$TGHOME/kegg_rdf/build_kegg.sh
-cp $TGHOME/kegg_rdf/rdf/kegg-pathways-genes.f.nt $AHOME
+$TGHOME/kegg_rdf/build_kegg.sh && \
+	cp $TGHOME/kegg_rdf/rdf/kegg-pathways-genes.f.nt $AHOME &
+
+wait
 
 $TGHOME/replace.sh $AHOME/kegg-pathways-genes.f.nt $REPO http://level-five.jp/t/annotation/kegg \
 	"KEGG pathways" "Updated $(date) from ftp.bioinformatics.jp"
