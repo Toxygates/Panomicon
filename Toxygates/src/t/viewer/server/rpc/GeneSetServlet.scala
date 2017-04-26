2b2eb445d96bd241cdf9e552c2594ec916a3f576
Johan Nystrom-Persson <jtnystrom@gmail.com>
2017-04-26 18:34:02 +0900

    Import probes by POST request - work in progress

---
+++ Toxygates/src/t/viewer/server/rpc/GeneSetServlet.scala
@@ -0,0 +1,75 @@
+package t.viewer.server.rpc
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
+
+import scala.collection.JavaConversions._
+import scala.io._
+import javax.servlet.ServletConfig
+import t.viewer.server.Configuration
+
+object GeneSetServlet { 
+  val IMPORT_SESSION_KEY = "importedGenes"
+}
+
+class GeneSetServlet extends HttpServlet {
+  import GeneSetServlet._
+  
+  
+  override def init(): Unit = {
+    super.init   
+  }
+  
+  override def destroy: Unit = {
+    super.destroy
+  }
+  
+  import HttpServletResponse._
+  
+  override def doPost(req: HttpServletRequest, resp: HttpServletResponse) = {
+    println(req.getPathInfo)
+    val request = Option(req.getPathInfo)
+    request match {
+      case Some(r) =>
+        r.split("/").toSeq match {
+          case "" +: "import" +: _ => doImport(req, resp)
+          case _ => 
+            Console.err.println(s"Unsupported request for GeneSetServlet: $request")
+            resp.setStatus(SC_BAD_REQUEST)
+        }
+      case _ =>
+        Console.err.println(s"Empty request for GeneSetServlet")
+        resp.setStatus(SC_BAD_REQUEST)
+    }            
+  }
+  
+  def doImport(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
+    val MAX_SIZE = 100000
+    req.getPathInfo
+    
+    val url = req.getRequestURL
+    val l = url.length() - ("geneSet/import").length()
+    
+    //example: "http://127.0.0.1:8888/toxygates/#data"
+    val REDIR_LOCATION = url.substring(0, l) + "#data"
+    
+    val is = req.getInputStream
+    val s = Source.fromInputStream(is)
+    val lines = s.getLines
+    val allGenes = 
+      (lines.flatMap(_.split(",").map(_.trim)) take MAX_SIZE).toArray 
+    
+    if (lines.hasNext) {
+      Console.err.println("Imported gene set is too large")
+      resp.setStatus(SC_BAD_REQUEST)
+      return
+    }
+    
+    println(s"Importing ${allGenes.size} genes")
+    req.getSession.setAttribute(IMPORT_SESSION_KEY, allGenes)
+    
+    resp.sendRedirect(REDIR_LOCATION)    
+  }
+  
+}
