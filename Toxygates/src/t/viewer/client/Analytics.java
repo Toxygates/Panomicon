c5822ea26ec920badb5d6eb90b2a1325773ac12d
Yuji Kosugi <yuji.kosugi@gmail.com>
2017-06-28 18:31:19 +0900

    Reorganize string constants hierarchically in Analytics.java

--- Toxygates/src/t/viewer/client/Analytics.java
+++ Toxygates/src/t/viewer/client/Analytics.java
@@ -5,21 +5,27 @@ public class Analytics {
   public static final String URL_PREFIX = "toxygates.html/";
 
   public static final String CATEGORY_ANALYSIS = "Analysis";
-  public static final String CATEGORY_TABLE = "Table";
-  public static final String CATEGORY_VISUALIZATION = "Visualization";
-  public static final String CATEGORY_GENE_SET = "Gene set";
-  public static final String CATEGORY_GENERAL = "General";
-
-  public static final String ACTION_DISPLAY_CHARTS = "Display charts";
-  public static final String ACTION_PAGE_CHANGE = "Page change";
   public static final String ACTION_COMPOUND_RANKING = "Compound ranking";
   public static final String ACTION_COMPOUND_RANKING_CHARTS = "Compound ranking charts";
-  public static final String ACTION_SAVE_SAMPLE_GROUP = "Save sample group";
-  public static final String ACTION_DISPLAY_OPTIONAL_COLUMN = "Display optional column";
   public static final String ACTION_PERFORM_ENRICHMENT = "Perform enrichment";
-  public static final String ACTION_PERFORM_CLUSTERING = "Perform clustering";
-  public static final String ACTION_MAGNIFY_CHART = "Magnify chart";
+  public static final String ACTION_PERFORM_UNIT_SEARCH = "Perform unit search";
+
+  public static final String CATEGORY_TABLE = "Table";
+  public static final String ACTION_PAGE_CHANGE = "Render/change page";
+  public static final String ACTION_DISPLAY_OPTIONAL_COLUMN = "Display optional column";
   public static final String ACTION_FILTER_COLUMN = "Enable/modify column filter";
+
+  public static final String CATEGORY_VISUALIZATION = "Visualization";
+  public static final String ACTION_DISPLAY_CHARTS = "Display charts";
+  public static final String ACTION_MAGNIFY_CHART = "Magnify chart";
+
+  public static final String CATEGORY_GENE_SET = "Gene set";
+
+  public static final String CATEGORY_GENERAL = "General";
+  public static final String ACTION_SAVE_SAMPLE_GROUP = "Save sample group";
+
+
+  public static final String ACTION_PERFORM_CLUSTERING = "Perform clustering"; // Not tracked yet
 
   /*
    * Tracks a pageview with Google Analytics if the google analytics script has been loaded.
