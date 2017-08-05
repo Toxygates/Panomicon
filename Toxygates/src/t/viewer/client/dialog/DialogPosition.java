62d6f7e0676d3e37622902564c364ce0c0d59098
Yuji Kosugi <yuji.kosugi@gmail.com>
2017-08-05 12:44:27 +0900

    Fix DialogPosition source formatting

--- Toxygates/src/t/viewer/client/dialog/DialogPosition.java
+++ Toxygates/src/t/viewer/client/dialog/DialogPosition.java
@@ -23,45 +23,49 @@ package t.viewer.client.dialog;
 import com.google.gwt.user.client.Window;
 
 public enum DialogPosition {
-	Center {
-		@Override
+  Center {
+    @Override
     public int computeX(int dialogWidth) {
-			return Window.getClientWidth()/2 - dialogWidth/2;
-		}
-		@Override
+      return Window.getClientWidth() / 2 - dialogWidth / 2;
+    }
+
+    @Override
     public int computeY(int dialogHeight) {
-			if (isTallDialog(dialogHeight)) {
-				return 50;
-			} else {
-				return Window.getClientHeight()/2 - dialogHeight/2;
-			}
-		}
-	},
-  Top {
-      @Override
-  public int computeX(int dialogWidth) {
-          return Window.getClientWidth()/2 - dialogWidth/2;
+      if (isTallDialog(dialogHeight)) {
+        return 50;
+      } else {
+        return Window.getClientHeight() / 2 - dialogHeight / 2;
       }
-      @Override
-  public int computeY(int dialogHeight) {
-              return 50;
     }
   },
-	Side {
-		@Override
+  Top {
+    @Override
     public int computeX(int dialogWidth) {
-			return Window.getClientWidth() - dialogWidth - 50;
-		}
-		@Override
+      return Window.getClientWidth() / 2 - dialogWidth / 2;
+    }
+
+    @Override
     public int computeY(int dialogHeight) {
-			return Center.computeY(dialogHeight);
-		}
-	};
-	
-	abstract public int computeX(int dialogWidth);
-	abstract public int computeY(int dialogHeight);
-	
-	public static boolean isTallDialog(int dialogHeight) {
-		return dialogHeight > Window.getClientHeight() - 100;
-	}
+      return 50;
+    }
+  },
+  Side {
+    @Override
+    public int computeX(int dialogWidth) {
+      return Window.getClientWidth() - dialogWidth - 50;
+    }
+
+    @Override
+    public int computeY(int dialogHeight) {
+      return Center.computeY(dialogHeight);
+    }
+  };
+
+  abstract public int computeX(int dialogWidth);
+
+  abstract public int computeY(int dialogHeight);
+
+  public static boolean isTallDialog(int dialogHeight) {
+    return dialogHeight > Window.getClientHeight() - 100;
+  }
 }
