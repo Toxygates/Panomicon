74ffaaa6bbf1d44bff719cd4aece0a5cb6909da9
Yuji Kosugi <yuji.kosugi@gmail.com>
2017-11-12 09:47:15 +0900

    Add column borders for Sample Search results

--- Toxygates/src/t/viewer/client/components/search/ResultTable.java
+++ Toxygates/src/t/viewer/client/components/search/ResultTable.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 import com.google.gwt.cell.client.Cell;
 import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.*;
 
 import otg.model.sample.OTGAttribute;
 import t.common.client.Utils;
@@ -117,14 +116,20 @@ public abstract class ResultTable<T> {
     addAttributeColumn(column, attribute);
   }
 
+  protected void addColumn(Column<T, ?> column, String title) {
+    TextHeader header = new TextHeader(title);
+    header.setHeaderStyleNames("lightBorderLeft");
+    cellTable().addColumn(column, header);
+  }
+
   protected void addAttributeColumn(AttributeColumn<T> column, Attribute attribute) {
     attributeColumns.put(attribute, column);
-    cellTable().addColumn(column, attribute.title());
+    addColumn(column, attribute.title());
   }
 
   protected void addNonAttributeColumn(Column<T, ?> column, String title) {
     nonAttributeColumns.add(column);
-    cellTable().addColumn(column, title);
+    addColumn(column, title);
   }
 
   public void setupTable(T[] entries, MatchCondition condition) {
@@ -194,6 +199,7 @@ public abstract class ResultTable<T> {
       super(cell);
       attribute = attrib;
       isNumeric = numeric;
+      setCellStyleNames("lightBorderLeft");
     }
 
     public Attribute attribute() {
