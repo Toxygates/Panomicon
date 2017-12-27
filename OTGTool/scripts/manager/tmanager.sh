0de377aaffbd0a806681a1d9699556587a9cbb38
Yuji Kosugi <yuji.kosugi@gmail.com>
2017-12-27 15:15:22 +0900

    Minor improvements to tmanager.sh

--- OTGTool/scripts/manager/tmanager.sh
+++ OTGTool/scripts/manager/tmanager.sh
@@ -3,8 +3,6 @@ MAIN_ROOT=/home/johan/ws/l5/Toxygates
 OTGTOOL_ROOT=$MAIN_ROOT/OTGTool
 TOXY_ROOT=$MAIN_ROOT/Toxygates
 
-SCALA_ROOT=/usr/local
-
 #This directory needs to contain libkyotocabinet and libjkyotocabinet
 KC_LIBDIR=/usr/local/lib
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$KC_LIBDIR"
@@ -14,18 +12,18 @@ CLASSPATH="${OTGTOOL_ROOT}/lib/jar/*:${TOXY_ROOT}/war/WEB-INF/lib/*:${OTGTOOL_RO
 export T_TS_URL=http://localhost:3030/Toxygates/query
 export T_TS_UPDATE_URL=http://localhost:3030/Toxygates/update
 
-#Forces read-only mode for triplestore when not set
+#Forces read-only mode for triplestore when unset
 #unset T_TS_UPDATE_URL
 
 export T_TS_USER=x
 export T_TS_PASS=y
 #export T_TS_REPO=ttest
 
-#export T_DATA_DIR=/home/johan/data_dev
+#export T_DATA_DIR=kcchunk:/home/johan/data_dev
 export T_DATA_DIR=kcchunk:/shiba/scratch/toxygates/rebuild_test
 export T_DATA_MATDBCONFIG="#pccap=1073741824#msiz=4294967296"
 
 echo $CLASSPATH
-exec $SCALA_ROOT/bin/scala -Djava.library.path=$KC_LIBDIR -J-Xmx4g -classpath $OTGTOOL_ROOT/bin:$CLASSPATH otg.Manager $@
+exec /usr/local/bin/scala -Djava.library.path=$KC_LIBDIR -J-Xmx4g -classpath $OTGTOOL_ROOT/bin:$CLASSPATH otg.Manager $@
 
 
