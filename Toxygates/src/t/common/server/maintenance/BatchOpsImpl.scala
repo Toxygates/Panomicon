387e8deeedad8a03f8e2af90108ebfe27834afd4
Yuji Kosugi <yuji.kosugi@gmail.com>
2018-01-24 09:48:27 +0900

    Stop using postfix operator notation to get rid of warning in BatchOpsImpl

--- Toxygates/src/t/common/server/maintenance/BatchOpsImpl.scala
+++ Toxygates/src/t/common/server/maintenance/BatchOpsImpl.scala
@@ -76,10 +76,10 @@ trait BatchOpsImpl extends MaintenanceOpsImpl
       val tempFiles = new TempFiles()
       setAttribute("tempFiles", tempFiles)
 
-      if (getFile(metaPrefix) == None) {
+      if (getFile(metaPrefix).isEmpty) {
         throw BatchUploadException.badMetaData("The metadata file has not been uploaded yet.")
       }
-      if (getFile(dataPrefix) == None) {
+      if (getFile(dataPrefix).isEmpty) {
         throw BatchUploadException.badNormalizedData("The normalized intensity file has not been uploaded yet.")
       }
 
