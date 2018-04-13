c90549671b9a7270e7e5ceb11076bce815192e8e
Johan Nystrom-Persson <jtnystrom@gmail.com>
2018-04-13 17:54:28 +0900

    FoldBuilder efficiency

--- OTGTool/src/main/scala/t/db/file/FoldBuilder.scala
+++ OTGTool/src/main/scala/t/db/file/FoldBuilder.scala
@@ -33,8 +33,10 @@ import scala.collection.{Map => CMap}
  * log-2 fold values constructed from the input data
  * The sample space of the output may be smaller (control samples have no folds)
  */
-abstract class FoldValueBuilder[E <: ExprValue](md: Metadata, input: RawExpressionData)
+abstract class FoldValueBuilder(md: Metadata, input: RawExpressionData)
   extends RawExpressionData {
+
+  type Triple = (Sample, String, FoldPExpr)
 
   def samples = input.samples.filter(!md.isControl(_))
 
@@ -44,9 +46,9 @@ abstract class FoldValueBuilder[E <: ExprValue](md: Metadata, input: RawExpressi
    * This method may return values for more samples than the one requested. Callers should
    * inspect the results for efficiency.
    */
-  def values(s: Sample): Seq[(Sample, String, E)] = {
+  def values(s: Sample): Seq[Triple] = {
     println("Compute control values")
-    var r = List[(Sample, String, E)]()
+    var r = List[Triple]()
     for ((ts, cs) <- groups;
       if ts.toSet.contains(s)) {
       println("Control barcodes: " + cs)
@@ -61,7 +63,7 @@ abstract class FoldValueBuilder[E <: ExprValue](md: Metadata, input: RawExpressi
    */
   protected def makeFolds(controlSamples: Seq[Sample],
       treatedSamples: Seq[Sample], sample: Sample,
-      accumulator: List[(Sample, String, E)]): List[(Sample, String, E)]
+      accumulator: List[Triple]): List[Triple]
 
   /**
    * Compute a control sample (as a mean).
@@ -103,8 +105,7 @@ abstract class FoldValueBuilder[E <: ExprValue](md: Metadata, input: RawExpressi
  * expression data for now.
  */
 class PFoldValueBuilder(md: Metadata, input: RawExpressionData)
-  extends FoldValueBuilder[PExprValue](md, input) {
-  type Triple = (Sample, String, PExprValue)
+  extends FoldValueBuilder(md, input) {
   val tt = new TTest
 
   //TODO: factor out some code shared with the superclass
@@ -144,9 +145,9 @@ class PFoldValueBuilder(md: Metadata, input: RawExpressionData)
           val controlCalls = controlSamples.flatMap(input.call(_, p))
           val treatedCalls = treatedSamples.flatMap(input.call(_, p))
           val pacall = foldPACall(foldVal, controlCalls, treatedCalls)
-          r ::= (sample, p, PExprValue(foldVal, pv, pacall))
+          r ::= (sample, p, (foldVal, pacall, pv))
         case _ =>
-          r ::= (sample, p, PExprValue(Double.NaN, Double.NaN, 'A'))
+          r ::= (sample, p, (Double.NaN, 'A', Double.NaN))
       }
     }
     r
@@ -156,7 +157,7 @@ class PFoldValueBuilder(md: Metadata, input: RawExpressionData)
 
   def data(s: Sample): CMap[String, FoldPExpr] = {
     val vs = values(s)
-    Map() ++ vs.filter(_._1 == s).map(v => v._2 -> (v._3.value, v._3.call, v._3.p))
+    Map() ++ vs.filter(_._1 == s).map(v => v._2 -> v._3)
   }
 }
 
@@ -199,9 +200,9 @@ object FoldBuilder extends CmdLineOptions {
       for ((p, vs) <- values; sorted = vs.toSeq.sortBy(s => samples.indexOf(s._1))) {
         for (w <- writers) { w.write("\"" + p + "\",") }
 
-        writer.write(sorted.map(_._3.value).mkString(","))
-        callwriter.write(sorted.map(x => "\"" + x._3.call + "\"").mkString(","))
-        pwriter.write(sorted.map(_._3.p).mkString(","))
+        writer.write(sorted.map(_._3._1).mkString(","))
+        callwriter.write(sorted.map(x => "\"" + x._3._2 + "\"").mkString(","))
+        pwriter.write(sorted.map(_._3._3).mkString(","))
 
         for (w <- writers) { w.newLine() }
       }
