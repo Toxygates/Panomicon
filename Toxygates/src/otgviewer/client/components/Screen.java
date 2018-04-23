db382ddb074a989cd17190563a682b15035ee61f
Yuji Kosugi <yuji.kosugi@gmail.com>
2018-04-23 10:33:28 +0900

    Move loadState from DataListenerWidget to Screen

--- Toxygates/src/otgviewer/client/components/Screen.java
+++ Toxygates/src/otgviewer/client/components/Screen.java
@@ -18,7 +18,11 @@
 
 package otgviewer.client.components;
 
+import static t.viewer.client.StorageParser.packProbes;
+import static t.viewer.client.StorageParser.unpackColumn;
+
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Nullable;
@@ -32,10 +36,10 @@ import com.google.gwt.resources.client.TextResource;
 import com.google.gwt.user.client.ui.*;
 
 import otgviewer.client.*;
-import t.common.shared.DataSchema;
-import t.common.shared.SharedUtils;
+import t.common.shared.*;
 import t.common.shared.sample.Group;
 import t.common.shared.sample.Sample;
+import t.model.SampleClass;
 import t.model.sample.AttributeSet;
 import t.viewer.client.*;
 import t.viewer.shared.AppInfo;
@@ -349,11 +353,75 @@ public class Screen extends DataListenerWidget implements
     deferredResize();
   }
 
-  @Override
+  /**
+   * Load saved state from the local storage. If the loaded state is different from what was
+   * previously remembered in this widget, the appropriate signals will fire.
+   */
+  public void loadState(Screen sc, AttributeSet attributes) {
+    StorageParser p = getParser(sc);
+    loadState(p, sc.schema(), attributes);
+  }
+
   public void loadState(StorageParser p, DataSchema schema, AttributeSet attributes) {
-    super.loadState(p, schema, attributes);
-    String v = p.getItem("OTG.showGuide");
-    if (v == null || v.equals("yes")) {
+    SampleClass sc = new SampleClass();
+    // Note: currently the "real" sample class, as chosen by the user on the
+    // column screen for example, is not stored, and hence not propagated
+    // between screens.
+    sampleClassChanged(sc);
+
+    try {
+      List<Group> cs = loadColumns(p, schema, "columns", chosenColumns, attributes);
+      if (cs != null) {
+        logger.info("Unpacked columns: " + cs.get(0) + ": "
+            + cs.get(0).getSamples()[0] + " ... ");
+        columnsChanged(cs);
+      }
+      Group g = unpackColumn(schema, p.getItem("customColumn"), attributes);
+      if (g != null) {
+        customColumnChanged(g);
+      }
+    } catch (Exception e) {
+      logger.log(Level.WARNING, "Unable to load state", e);
+      // one possible failure source is if data is stored in an incorrect
+      // format
+      columnsChanged(new ArrayList<Group>());
+      storeColumns(p); // overwrite the old data
+      storeCustomColumn(p, null); // ditto
+      logger.log(Level.WARNING, "Exception while parsing state", e);
+    }
+
+    String v = p.getItem("probes");
+    if (v != null && !v.equals("") && !v.equals(packProbes(chosenProbes))) {
+      chosenProbes = v.split("###");
+      probesChanged(chosenProbes);
+    } else if (v == null || v.equals("")) {
+      probesChanged(new String[0]);
+    }
+    List<ItemList> lists = loadItemLists(p);
+    if (lists.size() > 0) {
+      chosenItemLists = lists;
+      itemListsChanged(lists);
+    }
+
+    ItemList geneSet = ItemList.unpack(p.getItem("geneset"));
+    if (geneSet != null) {
+      chosenGeneSet = geneSet;
+    }
+    geneSetChanged(geneSet);
+
+    lists = loadClusteringLists(p);
+    if (lists.size() > 0) {
+      chosenClusteringList = lists;
+      clusteringListsChanged(lists);
+    }
+
+    // Note: the ordering of the following 3 is important
+    loadDatasets(p);
+    loadSampleClass(p, attributes);
+    loadCompounds(p);
+
+    String verbose = p.getItem("OTG.showGuide");
+    if (verbose == null || verbose.equals("yes")) {
       showGuide = true;
     } else {
       showGuide = false;
