e600b9c8fed2bb279d0e1656cbe7e4b3f41e7d2a
Yuji Kosugi <yuji.kosugi@gmail.com>
2018-07-09 15:11:28 +0900

    NetworkController: stop making interactions with null nodes * Fixes an issue where network visualization would fail because of null nodes * This was caused by the lookup table only containing nodes which are actually   visible in the dual table

--- Toxygates/src/t/viewer/client/network/NetworkController.java
+++ Toxygates/src/t/viewer/client/network/NetworkController.java
@@ -30,12 +30,14 @@ abstract public class NetworkController {
       for (String target: fullMap.get(mainProbe)) {        
         Node main = lookup.get(mainProbe);
         Node side = lookup.get(target);
-        
+
         //Directed interaction normally from miRNA to mRNA
         Node from = (!reversed) ? main : side;
         Node to = (!reversed) ? side : main;
-        Interaction i = new Interaction(from, to, null, null);
-        interactions.add(i);
+        if (from != null && to != null) {
+          Interaction i = new Interaction(from, to, null, null);
+          interactions.add(i);
+        }
       }
     }
     return new Network(title, nodes, interactions);
