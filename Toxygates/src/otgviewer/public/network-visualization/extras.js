882680f5f506bb4a2eaf1e60e5e9bfcbacb9d9b9
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2018-09-06 14:08:35 +0900

    Added checkbox to hide/display unconnected nodes and removed filter option from context menu

--- Toxygates/src/otgviewer/public/network-visualization/extras.js
+++ Toxygates/src/otgviewer/public/network-visualization/extras.js
@@ -4,18 +4,14 @@
 var convertedNetwork;
 
 /**
- * Initialization function , called by JQuery once the html of the site is
- * completely loaded
- */
-// $( function(){
-//   console.log("Site Loaded");
-// });
-
-/**
  * Convenience function used to load a file containing network data, stored
  * directly from toxygates on a JSON file.<br>
  */
 function loadFile(){
+
+  $("#display").width(1000);
+  $("#display").height(1000);
+
   var file = document.getElementById("fileSelect").files[0];
   // create an url and request the file
   var url = URL.createObjectURL(file);
@@ -25,9 +21,17 @@ function loadFile(){
     // loading of data finished correctly, thus we can proceed to its processing
     if( rqst.readyState == 4 && rqst.status == 200 ){
       convertedNetwork = JSON.parse(rqst.responseText);
+      console.log("cnet", convertedNetwork);
       // aqui es donde la ejecucion se pone a la par con lo que pasa en toxygates
       onReadyForVisualization();
     }
   }
   rqst.send();
 }
+
+/**
+ * Convenience function to see if the saving of network is working or not
+ */
+$(document).on("click", "#saveMe", function(event){
+  saveStuff();
+});
