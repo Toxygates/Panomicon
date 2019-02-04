daf1bef96de79475c4bdf7e96bd7441afc018d55
Yuji Kosugi <yuji.kosugi@gmail.com>
2019-02-04 12:51:56 +0900

    Move StackedListEditor.BrowseCheck and FreeEdit into their own files

---
+++ Toxygates/src/t/viewer/client/components/BrowseCheck.java
@@ -0,0 +1,110 @@
+package t.viewer.client.components;
+
+import java.util.*;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
+
+import t.common.client.components.StringSelectionTable;
+import t.viewer.client.Utils;
+
+/**
+ * A selection method for StackedListEditor that allows the user to browse a list and check 
+ * items with checkboxes. This is only recommended if the total number of available items 
+ * is small (< 1000).
+ */
+public class BrowseCheck extends SelectionMethod<String> {
+  protected StringSelectionTable selTable;
+  private DockLayoutPanel dlp = new DockLayoutPanel(Unit.PX);
+  private Button sortButton;
+  private ScrollPanel scrollPanel;
+
+  public BrowseCheck(StackedListEditor editor, String itemTitle, final int maxAutoSel) {
+    super(editor);
+    initWidget(dlp);
+
+    final BrowseCheck bc = this;
+    this.selTable = new StringSelectionTable("", itemTitle) {
+      @Override
+      protected void selectionChanged(Set<String> selected) {
+        //BrowseCheck.logger.info("Send selection " + selected.size());
+        parentSelector.setSelection(selected, bc);
+        currentSelection = selected;
+      }
+    };
+
+    HorizontalPanel hp = Utils.mkHorizontalPanel(true);
+    hp.setWidth("100%");
+    dlp.addSouth(hp, 36);
+
+    sortButton = new Button("Sort by name", new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent ce) {
+        List<String> items = new ArrayList<String>(parentSelector.availableItems());
+        Collections.sort(items);
+        setItems(items, false, true);
+        sortButton.setEnabled(false);
+      }
+    });
+    sortButton.addStyleName("lightButton");
+    hp.add(sortButton);
+    sortButton.setEnabled(false);
+
+    Button selectAllButton = new Button("Select all", new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        List<String> items = selTable.getItems();
+        List<String> sel = items;
+        if (items.size() > maxAutoSel) {
+          Window.alert("Too many items in list. Only the first " + maxAutoSel
+              + " will be selected.");
+          sel = items.subList(0, maxAutoSel);
+        }
+
+        setSelection(sel);
+        parentSelector.setSelection(sel, bc);
+      }
+    });
+    selectAllButton.addStyleName("lightButton");
+    hp.add(selectAllButton);
+
+    Button unselectAllButton = new Button("Unselect all", new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent ce) {
+        List<String> empty = new ArrayList<String>();
+        setSelection(empty);
+        parentSelector.setSelection(empty, bc);
+      }
+    });
+    unselectAllButton.addStyleName("lightButton");
+    hp.add(unselectAllButton);
+
+    scrollPanel = new ScrollPanel(selTable);
+    dlp.add(scrollPanel);
+  }
+
+  @Override
+  public String getTitle() {
+    return "Browse";
+  }
+
+  @Override
+  public void setSelection(Collection<String> items) {
+    // logger.info("Receive selection " + items.size());
+    selTable.setSelection(items);
+    selTable.table().redraw();
+  }
+
+  @Override
+  public void setItems(List<String> items, boolean clearSelection, boolean alreadySorted) {
+    selTable.setItems(items, clearSelection);
+    sortButton.setEnabled(!alreadySorted);
+  }
+
+  public void scrollToTop() {
+    scrollPanel.scrollToTop();
+  }
+}
---
+++ Toxygates/src/t/viewer/client/components/FreeEdit.java
@@ -0,0 +1,108 @@
+package t.viewer.client.components;
+
+import java.util.*;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
+
+import t.common.shared.SharedUtils;
+import t.viewer.client.Utils;
+
+/**
+ * A selection method for StackedListEditor that allows the user to edit a list as text, 
+ * freely. Items are separated by commas or whitespace.
+ */
+public class FreeEdit extends SelectionMethod<String> {
+  // TODO: the constants 10, 45 are somewhat ad-hoc -- find a better method in the future
+  protected TextArea textArea = new ResizableTextArea(10, 45);
+  private String lastText = "";
+  private Timer t;
+  private DockLayoutPanel dlp;
+  private HorizontalPanel np;    
+
+  public FreeEdit(StackedListEditor editor) {
+    super(editor);
+    dlp = new DockLayoutPanel(Unit.PX);
+    initWidget(dlp);
+
+    Label l = new Label("Search:");
+    final SuggestBox sb = new SuggestBox(new SuggestOracle() {
+      @Override
+      public void requestSuggestions(Request request, Callback callback) {
+        callback.onSuggestionsReady(request,
+            new Response(parentSelector.getSuggestions(request.getQuery())));
+      }
+    });
+    HorizontalPanel hp = Utils.mkHorizontalPanel(true, l, sb);
+    np = Utils.mkWidePanel();
+    np.add(hp);
+
+    sb.addSelectionHandler(new SelectionHandler<SuggestOracle.Suggestion>() {
+      @Override
+      public void onSelection(SelectionEvent<Suggestion> event) {
+        Suggestion s = event.getSelectedItem();
+        String selection = s.getDisplayString();
+        String oldText = textArea.getText().trim();
+        String newText = (!"".equals(oldText)) ? (oldText + "\n" + selection) : selection;
+        textArea.setText(newText);
+        refreshItems(true);
+        sb.setText("");
+      }
+    });
+
+    dlp.addNorth(np, 36);
+    textArea.setSize("100%", "100%");
+    dlp.add(textArea);
+    t = new Timer() {
+      @Override
+      public void run() {
+        refreshItems(false);
+      }
+    };
+
+    textArea.addKeyUpHandler(new KeyUpHandler() {
+      @Override
+      public void onKeyUp(KeyUpEvent event) {
+        lastText = textArea.getText();
+        t.schedule(500);
+      }
+    });
+  }
+
+  private void refreshItems(boolean immediate) {
+    final FreeEdit fe = this;
+    // Without the immediate flag, only do the refresh action if
+    // the text has been unchanged for 500 ms.
+    if (immediate || lastText.equals(textArea.getText())) {
+      String[] items = parseItems();
+      Set<String> valid = parentSelector.validateItems(Arrays.asList(items));
+      if (!parentSelector.getSelection().equals(valid)) {
+        parentSelector.setSelection(valid, fe);
+      }
+      currentSelection = valid;
+    }
+  }
+
+  @Override
+  public String getTitle() {
+    return "Edit/paste";
+  }
+
+  private String[] parseItems() {
+    String s = textArea.getText();
+    String[] split = s.split("\\s*[,\n]+\\s*");
+    return split;
+  }
+
+  @Override
+  public void setSelection(Collection<String> items) {
+    super.setSelection(items);
+    textArea.setText(SharedUtils.mkString(items, "\n"));
+  }
+}
