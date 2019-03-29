f79e4c1bb3b793bcb0559375fa44ae32ed5fdbc2
Johan Nystrom-Persson <jtnystrom@gmail.com>
2019-03-29 17:57:56 +0900

    Load mirna target files in futures, to speed up application startup

--- Toxygates/src/otg/viewer/server/rpc/NetworkServiceImpl.scala
+++ Toxygates/src/otg/viewer/server/rpc/NetworkServiceImpl.scala
@@ -6,32 +6,56 @@ import t.platform.mirna.MiRDBConverter
 import t.platform.mirna.TargetTable
 import t.platform.mirna.TargetTableBuilder
 import t.viewer.shared.mirna.MirnaSource
+import t.viewer.server.Configuration
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.blocking
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Failure
+import scala.util.Success
 
 class NetworkServiceImpl extends t.viewer.server.rpc.NetworkServiceImpl
   with OTGServiceServlet {
 
-  protected def tryReadTargetTable(file: String, doRead: String => TargetTable) =
-    try {
+  var mirdbTableFut: Future[TargetTable] = _
+  var mirtarbaseTableFut: Future[TargetTable] = _
+
+  override def localInit(c: Configuration) {
+    super.localInit(c)
+    mirdbTableFut = Future {
+      readTargetTable(
+        s"$mirnaDir/mirdb_filter.txt",
+        new MiRDBConverter(_, "MiRDB 5.0").makeTable)
+    }
+    mirtarbaseTableFut = Future {
+       readTargetTable(
+      s"$mirnaDir/tm_mirtarbase.txt",
+      MiRNATargets.tableFromFile(_))
+    }
+  }
+
+  protected def readTargetTable(file: String, doRead: String => TargetTable) = {
+    println(s"Begin reading $file")
+    blocking {
       val t = doRead(file)
       println(s"Read ${t.size} miRNA targets from $file")
-      Some(t)
-    } catch {
-      case e: Exception =>
-        e.printStackTrace()
-        None
+      t
     }
-
-  lazy val mirdbTable =
-    tryReadTargetTable(
-      s"$mirnaDir/mirdb_filter.txt",
-      new MiRDBConverter(_, "MiRDB 5.0").makeTable)
+  }
 
   def reverse[A,B](xs: Map[A,B]) = xs.map(x => (x._2 -> x._1))
 
-  lazy val mirtarbaseTable =
-    tryReadTargetTable(
-      s"$mirnaDir/tm_mirtarbase.txt",
-      MiRNATargets.tableFromFile(_))
+  def resultAsOption[T](fut: Future[T]) = {
+    val r = Await.ready(fut, Duration.Inf).value.get
+    r match {
+      case Success(t) => Some(t)
+      case Failure(e) => None
+    }
+  }
+
+  def mirtarbaseTable = resultAsOption(mirtarbaseTableFut)
+  def mirdbTable = resultAsOption(mirdbTableFut)
 
   protected def mirnaTargetTable(source: MirnaSource) = {
     val table = source.id match {
