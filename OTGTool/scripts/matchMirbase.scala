6f335419c2c591c58234856ad55879c1a6a8fc62
Johan Nystrom-Persson <jtnystrom@gmail.com>
2019-08-23 17:30:38 +0900

    Generalize matchRfam.scala to work on various external microrna platform formats, not just Rfam

--- OTGTool/scripts/matchMirbase.scala
+++ OTGTool/scripts/matchMirbase.scala
@@ -1,12 +1,12 @@
 /**
-* Script to convert RFam probes into mirbase based on the mature RNA's DNA sequence.
-* Needs a reference mirbase file to perform the matching.
-* 
-* Example usage: 1. reverse transcribe mirbase:
-* zcat mirbase-v21-mature.fa.gz | scala transcribeMirbase.scala > mirbase-v21-mature-dna.fa
-* 
-* scala matchRfam.scala mirbase-v21-mature-dna.fa GPL8225-tbl-1.txt GSE13874-GPL8225_series_matrix.data.csv
-*/
+ * Script to convert probes from other/older platforms (e.g. Rfam) into mirbase based on the mature RNA's DNA sequence.
+ * Needs a reference mirbase file to perform the matching.
+ *
+ * Example usage: 1. reverse transcribe mirbase:
+ * zcat mirbase-v21-mature.fa.gz | scala transcribeMirbase.scala > mirbase-v21-mature-dna.fa
+ *
+ * scala matchMirbase.scala mirbase-v21-mature-dna.fa GPL8225-tbl-1.txt GSE13874-GPL8225_series_matrix.data.csv
+ */
 
 import scala.io._
 import java.io._
@@ -34,7 +34,7 @@ def bestMatches(dnaSeq: String): Seq[String] = {
     case Some(e) => e
     case None =>
       //Look up entries where one sequence is fully contained in the other
-      val partial = mirnaDNA.filter(mirna => 
+      val partial = mirnaDNA.filter(mirna =>
         mirna._1.contains(dnaSeq) || dnaSeq.contains(mirna._1))
       if (!partial.isEmpty) {
         println(s"Partial match: $dnaSeq -> ${partial.map(_._1)}")
@@ -43,16 +43,36 @@ def bestMatches(dnaSeq: String): Seq[String] = {
   }
 }
 
-val rfamPlatform = Source.fromFile(args(1)).getLines
+def complement(dna: String) = dna.map {
+  case 'T' => 'A'
+  case 'A' => 'T'
+  case 'C' => 'G'
+  case 'G' => 'C'
+  case c => c
+}
+def reverse(dna: String) = dna.reverse
+
+val other = Source.fromFile(args(1)).getLines
 var mapping = Map[String, Seq[String]]()
 var success = 0
 var fail = 0
 var total = 0
-for (l <- rfamPlatform; spl = l.split("\t");
-  id = spl(0); fullId = spl(2); dnaSequence = spl(4)) {
-  val best = bestMatches(dnaSequence).toList
-  println(s"$fullId => $best")
-  if (best.isEmpty) { 
+
+//Redefine this function to adjust the matching behaviour
+def applyTransforms(dna: String) = reverse(complement(dna))
+
+//This column will be used to find the DNA sequence used for lookup
+val probeSeqCol = 5
+//This column will be used to find the ID that is to be translated into mirbase
+val idCol = 0
+
+for (l <- other; spl = l.split("\t");
+     id = spl(idCol); dnaSequence = spl(probeSeqCol)) {
+  val candidate = applyTransforms(dnaSequence)
+
+  val best = bestMatches(candidate).toList
+  println(s"$id => $best")
+  if (best.isEmpty) {
     fail += 1
   } else {
     mapping += (id -> best)
@@ -62,15 +82,15 @@ for (l <- rfamPlatform; spl = l.split("\t");
 }
 println(s"Success $success fail $fail total range $total")
 
-val w = new PrintWriter(args(2) + "_rfamTranslate.csv")
+val w = new PrintWriter(args(2) + "_mirbaseTranslate.csv")
 val input = Source.fromFile(args(2)).getLines
 w.println(input.next)
 for (
   l <- input;
-  spl = l.split(",",2);
-  txns <- mapping.get(spl(0));
+  spl = l.split(",", 2);
+  txns <- mapping.get(spl(0).replace("\"", ""));
   txn <- txns
-  ) {
+) {
   w.println("\"" + s"$txn" + "\"," + spl(1))
 }
 
