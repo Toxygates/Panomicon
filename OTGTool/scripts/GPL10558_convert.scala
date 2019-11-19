8360b1f899f57e939975024e8a1f0b58b4112ddc
Johan Nystrom-Persson <jtnystrom@gmail.com>
2019-11-19 15:05:00 +0900

    Improvements to GPL10558 import script

--- OTGTool/scripts/GPL10558_convert.scala
+++ OTGTool/scripts/GPL10558_convert.scala
@@ -5,15 +5,21 @@ val in = Source.stdin.getLines
 def optValue(x: String) = 
 	if (x.trim.length > 0) Some(x) else None
 
+def trimRefseq(x: String): String =
+	x.split("\\.")(0)
+
+def makeTitle(x: String): String =
+	x.split("\\.")(0).replace(",","")
 
 for (x <- in) {
 	val xs = x.split("\t", -1)
-	if (xs.length >= 14) {
+	if (xs.length >= 23) {
 		val attrs = Seq(
-			optValue(xs(6)).map("refseqTrn=" + _),
+			optValue(xs(6)).map(r => "refseqTrn=" + trimRefseq(r)),
 			optValue(xs(7)).map("unigene=" + _),
 			optValue(xs(8)).map("entrez=" + _),
-			optValue(xs(11)).map("symbol=" + _)
+			optValue(xs(11)).map("symbol=" + _),
+			optValue(xs(22)).map(t => "title=" + makeTitle(t))
 		)
 		val id = xs(13)
 		print(s"$id\t")
