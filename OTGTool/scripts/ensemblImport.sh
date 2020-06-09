f80ddda560460ec3a0e68091446c8785852892ac
Johan Nystrom <johan@monomorphic.org>
2020-06-09 15:05:35 +0900

    Update ensemblImport.sh to also import rat and mouse platforms

--- OTGTool/scripts/ensemblImport.sh
+++ OTGTool/scripts/ensemblImport.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 #Script to filter ensembl annotations (RDF) to generate a platform in Panomicon - work in progress
+#We filter the raw annotation files from ensembl mainly to keep the sizes manageable.
 
-egrep "prefix|refseq|uniprot|ncbigene|identifiers/go" homo_sapiens_xrefs.ttl > homo_sapiens_xrefs_filtered.ttl
-zcat homo_sapiens.ttl.gz | grep SO_transcribed_from >> homo_sapiens_xrefs_filtered.ttl
-zcat homo_sapiens.ttl.gz | grep SO_translates_to >> homo_sapiens_xrefs_filtered.ttl
+#After these files have been generated, they must be inserted into the triplestore manually (for example using curl,
+#or the replace.sh script) and then the TManager tool should be run to add the corresponding platform.
 
+function process_species {
+  SPECIES=$1
+  OUT=${SPECIES}_filtered.ttl
+  shift
+  zcat ${SPECIES}_xrefs.ttl.gz | egrep "prefix|refseq|uniprot|ncbigene|identifiers/go" > $OUT
+  zcat $SPECIES.ttl.gz | grep SO_transcribed_from >> $OUT
+  zcat $SPECIES.ttl.gz | grep SO_translates_to >> $OUT
+}
 
+process_species homo_sapiens
+process_species mus_musculus
+process_species rattus_norvegicus
