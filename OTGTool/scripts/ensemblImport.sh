c2d7ac871f653ae79caa6efbddf7ccdea6f53b17
Johan Nystrom <johan@monomorphic.org>
2020-06-25 18:12:09 +0900

    More complete, integrated ensemblImport.sh to do all the necessary steps for creating Ensembl platforms

--- OTGTool/scripts/ensemblImport.sh
+++ OTGTool/scripts/ensemblImport.sh
@@ -3,18 +3,51 @@
 #Script to filter ensembl annotations (RDF) to generate a platform in Panomicon - work in progress
 #We filter the raw annotation files from ensembl mainly to keep the sizes manageable.
 
-#After these files have been generated, they must be inserted into the triplestore manually (for example using curl,
-#or the replace.sh script) and then the TManager tool should be run to add the corresponding platform.
+#You must configure scripts/triplestore/replace.sh with the correct URL, username and password for the
+#triplestore (e.g. Fuseki) for this to work.
+#It is also necessary to configure functions.sh (general settings) and manager/tmanager.sh.
+
+#Triplestore repository
+REPO=$1
+shift
+
+BASEURL=ftp://ftp.ensembl.org/pub/current_rdf
+DIR=downloads
+
+function species_shortcode {
+  #If new species are added, also add the code to (OTGTool) t/platform/Species.scala
+  case "$1" in
+    homo_sapiens      ) echo "hsa" ;;
+    mus_musculus      ) echo "mmu" ;; 
+    rattus_norvegicus ) echo "rno" ;;
+    * ) exit 1 ;;
+  esac
+}
 
 function process_species {
   SPECIES=$1
+
+  FILE=${SPECIES}.ttl.gz
+  XREF=${SPECIES}_xrefs.ttl.gz
+  pushd $DIR
+  curl -O $BASEURL/$SPECIES/$FILE
+  curl -O $BASEURL/$SPECIES/$XREF
+
   OUT=${SPECIES}_filtered.ttl
-  shift
-  zcat ${SPECIES}_xrefs.ttl.gz | egrep "prefix|refseq|uniprot|ncbigene|identifiers/go" > $OUT
-  zcat $SPECIES.ttl.gz | grep SO_transcribed_from >> $OUT
-  zcat $SPECIES.ttl.gz | grep SO_translates_to >> $OUT
+  zcat $XREF | egrep "prefix|refseq|uniprot|ncbigene|identifiers/go" > $OUT
+  zcat $FILE | grep SO_transcribed_from >> $OUT
+  zcat $FILE | grep SO_translates_to >> $OUT
+
+  popd
+
+  STITLE=$(species_shortcode $SPECIES).ensembl
+  GRAPH=http://level-five.jp/t/platform/$STITLE
+  TITLE="\"$SPECIES Ensembl latest $(date)\""
+  ./triplestore/replace.sh $DIR/$OUT $REPO $GRAPH $TITLE
+  ./manager/tmanager.sh platform addEnsembl -title $STITLE -input $DIR/$OUT
 }
 
+mkdir -p $DIR
 process_species homo_sapiens
 process_species mus_musculus
 process_species rattus_norvegicus
