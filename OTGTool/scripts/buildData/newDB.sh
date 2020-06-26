290d960c4c6d2c7baf397a3eb483c3180c2abff5
Johan Nystrom-Persson <jtnystrom@gmail.com>
2020-06-26 15:24:49 +0900

    newDB.sh work in progress

--- OTGTool/scripts/buildData/newDB.sh
+++ OTGTool/scripts/buildData/newDB.sh
@@ -4,7 +4,8 @@
 BASE=$(dirname $0)/..
 source $BASE/config.sh
 
-cd $T_DATA_PATH
+mkdir -p $T_DATA_PATH
+pushd $T_DATA_PATH
 
 #The size parameters here have been found by experiment to be efficient for 
 #a database that contains all of Open TG-GATEs and some additional data.
@@ -14,10 +15,12 @@ cd $T_DATA_PATH
 kchashmgr create -bnum 8000000 expr.kch
 kchashmgr create -bnum 8000000 fold.kch
 
-kctreemgr create -bnum 2000000 expr.kch
-kctreemgr create -bnum 2000000 fold.kch
+kctreemgr create -bnum 2000000 time_series.kct
+kctreemgr create -bnum 2000000 dose_series.kct
 
 #Smaller databases, less sensitive
 kctreemgr create -bnum 100000 probe_index.kct
 kctreemgr create -bnum 50000 enum_index.kct
+
+popd
 
