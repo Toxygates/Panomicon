aeabea8972d1897b4796d83c9c66908bfa625bbc
Johan Nystrom-Persson <jtnystrom@gmail.com>
2020-06-26 14:16:46 +0900

    Scripts for building a new database - work in progress

--- OTGTool/scripts/manager/tmanager.sh
+++ OTGTool/scripts/manager/tmanager.sh
@@ -4,11 +4,6 @@ source $(dirname $0)/../config.sh
 #Forces read-only mode for triplestore when unset
 #unset T_TS_UPDATE_URL
 
-#export T_DATA_DIR=kcchunk:/home/johan/data_dev
-export T_DATA_DIR=kcchunk:/shiba/scratch/toxygates/rebuild_test
-export T_DATA_MATDBCONFIG="#msiz=4294967296"
-
-echo $FULL_CLASSPATH
 runfull -Djava.library.path=$KC_LIBDIR -J-Xmx4g otg.Manager "$@"
 
 
