b9b522cb6e910a6e9f5ead23d832433081a3a9ca
Yuji Kosugi <yuji.kosugi@gmail.com>
2020-07-07 13:08:24 +0900

    Remove LinkingColumn.wasSizeExceeded and just base truncation on MAX_SIZE

--- Toxygates/src/t/viewer/client/table/LinkingColumn.java
+++ Toxygates/src/t/viewer/client/table/LinkingColumn.java
@@ -19,14 +19,14 @@
 
 package t.viewer.client.table;
 
-import java.util.*;
-
-import javax.annotation.Nullable;
-
 import com.google.gwt.cell.client.SafeHtmlCell;
-
 import t.common.shared.SharedUtils;
 import t.viewer.shared.AssociationValue;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public abstract class LinkingColumn<T> extends HTMLHideableColumn<T> {
   public LinkingColumn(SafeHtmlCell c, String name, boolean initState, String width,
@@ -42,7 +42,7 @@ public abstract class LinkingColumn<T> extends HTMLHideableColumn<T> {
 
   // Note: might move this method down or parameterise AssociationValue,
   // use an interface etc
-  protected List<String> makeLinks(Collection<AssociationValue> values, boolean limitExceeded) {
+  protected List<String> makeLinks(Collection<AssociationValue> values) {
     List<String> r = new ArrayList<String>();
     int i = 0;
     for (AssociationValue v : values) {
@@ -59,7 +59,7 @@ public abstract class LinkingColumn<T> extends HTMLHideableColumn<T> {
       }
       if (i == MAX_ITEMS + 1) {
         r.add("<div> ... (" + values.size() +
-                ( limitExceeded ? "+ items" : " items") +
+                ( values.size() > MAX_ITEMS ? "+ items" : " items") +
             ")");
       }
     }
@@ -68,16 +68,11 @@ public abstract class LinkingColumn<T> extends HTMLHideableColumn<T> {
 
   @Override
   protected String getHtml(T row) {
-    return SharedUtils.mkString(makeLinks(getLinkableValues(row),
-        wasSizeExceeded(row)), "");
+    return SharedUtils.mkString(makeLinks(getLinkableValues(row)), "");
   }
 
   protected Collection<AssociationValue> getLinkableValues(T data) {
     return new ArrayList<AssociationValue>();
-  }
-
-  protected boolean wasSizeExceeded(T data) {
-    return false;
   }
 
   protected abstract String formLink(String value);
