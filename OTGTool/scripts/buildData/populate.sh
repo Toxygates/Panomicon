50afdd72724c67f5efad15c453a1b3816070f085
Johan Nystrom-Persson <jtnystrom@gmail.com>
2020-07-14 14:15:03 +0900

    Basic initial data file for populating a blank triplestore

--- OTGTool/scripts/buildData/populate.sh
+++ OTGTool/scripts/buildData/populate.sh
@@ -10,6 +10,12 @@ BASE=$(dirname $0)/..
 source $BASE/config.sh
 
 cd $BASE
+
+#Insert basic initial data into the triplestore (definitions of a default instance and dataset)
+MIME="application/x-turtle"
+curl -u $T_TS_USER:$T_TS_PASS -X POST -H "Content-type:$MIME" "$T_TS_ROOT/$REPO/data" --data-binary @buildData/initData.ttl
+
+#Import data from various sources
 ./ensemblImport.sh
 ./mirnaImport.sh
 rdf/update_goterms.sh
