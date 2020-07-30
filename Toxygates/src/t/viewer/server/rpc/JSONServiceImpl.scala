d2c0df8058552e8c2ef6902597ebacf22b2bc1b5
Johan Nystrom <johan@monomorphic.org>
2020-07-30 13:02:50 +0900

    JSON service experiments (obtain samples, handle large outputs)

--- Toxygates/src/t/viewer/server/rpc/JSONServiceImpl.scala
+++ Toxygates/src/t/viewer/server/rpc/JSONServiceImpl.scala
@@ -1,5 +1,6 @@
 package t.viewer.server.rpc
 
+import java.io.PrintWriter
 import java.util.Date
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
@@ -9,6 +10,18 @@ import t.viewer.server.{Configuration, SharedDatasets}
 import upickle.default._
 import upickle.default.{macroRW, ReadWriter => RW}
 
+
+package json {
+  object Dataset {
+    //Needed for upickle to convert this class to/from JSON
+    implicit val rw: RW[Dataset] = macroRW
+  }
+  case class Dataset(id: String, title: String, numBatches: Int)
+
+  object Sample { implicit val rw: RW[Sample] = macroRW }
+  case class Sample(id: String, `type`: String, platform: String)
+}
+
 class JSONServiceImpl extends OTGServiceServlet {
 
   var sampleFilter: SampleFilter = _
@@ -17,20 +30,14 @@ class JSONServiceImpl extends OTGServiceServlet {
     sampleFilter = SampleFilter(conf.instanceURI)
   }
 
-  object Dataset {
-    //Needed for upickle to convert this class to/from JSON
-    implicit val rw: RW[Dataset] = macroRW
-  }
-  case class Dataset(id: String, title: String, numBatches: Int)
-
   private def datasets =
     (new Datasets(baseConfig.triplestore) with SharedDatasets).sharedList.map(d => {
-      Dataset(d.getId, d.getUserTitle, d.getNumBatches)
+      json.Dataset(d.getId, d.getUserTitle, d.getNumBatches)
     }).toSeq
 
   lazy val sampleStore = context.sampleStore
 
-  def isDataVisible(data: Dataset, userKey: String) = {
+  def isDataVisible(data: json.Dataset, userKey: String) = {
       data.id == t.common.shared.Dataset.userDatasetId(userKey) ||
         t.common.shared.Dataset.isSharedDataset(data.id) ||
         !data.id.startsWith("user-")
@@ -40,7 +47,7 @@ class JSONServiceImpl extends OTGServiceServlet {
     val reqId = Option(req.getParameter("id"))
     val userKey = Option(req.getParameter("userKey")).getOrElse("")
 
-    val out = resp.getWriter
+    val out = new PrintWriter(resp.getOutputStream)
     val data = reqId match {
       case Some(id) => datasets.filter(_.id == id)
       case _ => datasets.filter(isDataVisible(_, userKey))
@@ -53,7 +60,7 @@ class JSONServiceImpl extends OTGServiceServlet {
 
   def getParameterValues(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
     val param = Option(req.getParameter("param")).getOrElse(throw new Exception("Please specify parameter"))
-    val out = resp.getWriter
+    val out = new PrintWriter(resp.getOutputStream)
     val attr = baseConfig.attributes.byId(param)
     val values = sampleStore.attributeValues(SampleClassFilter().filterAll,
       attr, sampleFilter).toArray
@@ -62,8 +69,26 @@ class JSONServiceImpl extends OTGServiceServlet {
     out.flush()
   }
 
+  def getSamples(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
+    import t.model.sample.CoreParameter._
+
+    val limit = Option(req.getParameter("limit"))
+
+    val out = new PrintWriter(resp.getOutputStream)
+    val samples = sampleStore.sampleQuery(SampleClassFilter(), sampleFilter)().map(s =>
+      json.Sample(s.sampleId, s.sampleClass(Type), s.sampleClass(Platform)))
+
+    limit match {
+      case Some(l) =>
+        out.println(write(samples take l.toInt))
+      case None =>
+        out.println(write(samples))
+    }
+    out.flush()
+  }
+
   def getTime(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
-    val out = resp.getWriter
+    val out = new PrintWriter(resp.getOutputStream)
     out.println("Invalid request. Current time:")
     out.println(new Date())
     out.flush()
@@ -73,6 +98,7 @@ class JSONServiceImpl extends OTGServiceServlet {
     Option(req.getPathInfo) match {
       case Some("/datasets") => getDatasets(req, resp)
       case Some("/parameter") => getParameterValues(req, resp)
+      case Some("/samples") => getSamples(req, resp)
       case _ => getTime(req, resp)
     }
   }
