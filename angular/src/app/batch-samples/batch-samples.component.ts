9b37d62c7d2ffc8297e788c1ab11906bfdf8a2c4
Yuji Kosugi <yuji.kosugi@gmail.com>
2020-10-23 12:58:55 +0900

    Switch to primeng from ngx-bootstrap Virtual scrolling and better styling for batch samples table

--- angular/src/app/batch-samples/batch-samples.component.ts
+++ angular/src/app/batch-samples/batch-samples.component.ts
@@ -12,8 +12,26 @@ export class BatchSamplesComponent implements OnInit {
 
   samples: any;
   batchId: string;
+  virtualScroll: boolean;
+
+  cols: any[];
+  tableWidth: Number;
 
   ngOnInit(): void {
+    this.cols = [
+      {field: 'sample_id', header: 'Sample ID', width: "300"},
+      {field: 'type', header: 'Type', width: "75"},
+      {field: 'organism', header: 'Organism', width: "75"},
+      {field: 'test_type', header: 'Test type', width: "75"},
+      {field: 'organ_id', header: 'Organ', width: "75"},
+      {field: 'compound_name', header: 'Compound', width: "125"},
+      {field: 'sin_rep_type', header: 'Repeat?', width: "75"},
+      {field: 'dose_level', header: 'Dose level', width: "75"},
+      {field: 'exposure_time', header: 'Exposure time', width: "75"},
+      {field: 'platform_id', header: 'Platform ID', width: "85"},
+      {field: 'control_group', header: 'Control group', width: "85"}
+    ];
+    this.tableWidth = this.cols.reduce((sum, v) => sum + +v.width, 0) + 100;
   }
 
   loadSamplesForBatch(batchId: string) {
@@ -23,6 +41,7 @@ export class BatchSamplesComponent implements OnInit {
       .subscribe(
         result => {
           this.samples = result;
+          this.virtualScroll = this.samples.length > 50;
         }
       )
   }
