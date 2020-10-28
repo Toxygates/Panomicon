7c38e49dd5896af06ffa59857a739148db6c411b
Yuji Kosugi <yuji.kosugi@gmail.com>
2020-10-28 16:40:50 +0900

    Switch back to bootstrap from primeng
    Mostly just reverting commit 9b37d62c7d2ffc8297e788c1ab11906bfdf8a2c4.

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,78 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { MenuItem, PrimeNGConfig } from 'primeng/api';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'Panomicon';
-  
-  items: MenuItem[];
-
-  constructor(private primengConfig: PrimeNGConfig) {}
-
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-
-    this.items = [
-      {
-          label:'File',
-          icon:'pi pi-fw pi-file',
-          items:[
-              {
-                  label:'New',
-                  icon:'pi pi-fw pi-plus',
-                  items:[
-                  {
-                      label:'Bookmark',
-                      icon:'pi pi-fw pi-bookmark'
-                  },
-                  {
-                      label:'Video',
-                      icon:'pi pi-fw pi-video'
-                  },
-
-                  ]
-              },
-              {
-                  label:'Delete',
-                  icon:'pi pi-fw pi-trash'
-              },
-              {
-                  separator:true
-              },
-              {
-                  label:'Export',
-                  icon:'pi pi-fw pi-external-link'
-              }
-          ]
-      },
-      {
-          label:'Edit',
-          icon:'pi pi-fw pi-pencil',
-          items:[
-              {
-                  label:'Left',
-                  icon:'pi pi-fw pi-align-left'
-              },
-              {
-                  label:'Right',
-                  icon:'pi pi-fw pi-align-right'
-              },
-              {
-                  label:'Center',
-                  icon:'pi pi-fw pi-align-center'
-              },
-              {
-                  label:'Justify',
-                  icon:'pi pi-fw pi-align-justify'
-              },
-
-          ]
-      }
-    ];
-  }
+export class AppComponent {
+  title = 'Panomicon Angular test';
 }
