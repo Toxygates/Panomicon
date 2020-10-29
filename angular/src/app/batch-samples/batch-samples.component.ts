cd581ce3866f33553eeec103077782e56a3463fa
Yuji Kosugi <yuji.kosugi@gmail.com>
2020-10-29 10:39:16 +0900

    Angular: switch to tabulator for tables

--- angular/src/app/batch-samples/batch-samples.component.ts
+++ angular/src/app/batch-samples/batch-samples.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../backend.service';
+import Tabulator from 'tabulator-tables';
 
 @Component({
   selector: 'app-batch-samples',
@@ -13,6 +14,22 @@ export class BatchSamplesComponent implements OnInit {
   samples: any;
   batchId: string;
 
+  columns = [
+    {title: 'Sample ID', field: 'sample_id'},
+    {title: 'Type', field: 'type'},
+    {title: 'Organism', field: 'organism'},
+    {title: 'Test type', field: 'test_type'},
+    {title: 'Organ', field: 'organ_id'},
+    {title: 'Compound', field: 'compound_name'},
+    {title: 'Repeat?', field: 'sin_rep_type'},
+    {title: 'Dose level', field: 'dose_level'},
+    {title: 'Exposure time', field: 'exposure_time'},
+    {title: 'Platform ID', field: 'platform_id'},
+    {title: 'Control group', field: 'control_group'}
+  ]
+
+  tab = document.createElement('div');
+
   ngOnInit(): void {
   }
 
@@ -23,8 +40,19 @@ export class BatchSamplesComponent implements OnInit {
       .subscribe(
         result => {
           this.samples = result;
+          this.drawTable();
         }
       )
   }
 
+  private drawTable(): void {
+    new Tabulator("#my-tabular-table", {
+      data: this.samples,
+      //reactiveData:true, //enable data reactivity
+      columns: this.columns,
+      layout:"fitDataTable",
+      //layout: 'fitData',
+      maxHeight: "75vh"
+    });
+  }
 }
