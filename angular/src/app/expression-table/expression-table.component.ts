b4c725036aa7b0f4f558e7a4e8ad63a7960eae9b
Yuji Kosugi <yuji.kosugi@gmail.com>
2020-11-16 11:16:03 +0900

    Angular: add very basic expression table functionality

--- angular/src/app/expression-table/expression-table.component.ts
+++ angular/src/app/expression-table/expression-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BackendService } from '../backend.service';
+import Tabulator from 'tabulator-tables';
 
 import { filter } from 'rxjs/operators';
 
@@ -15,19 +16,55 @@ export class ExpressionTableComponent implements OnInit {
     private backend: BackendService) { }
 
   samples: string[] = [];
-  matrix: any;
+  data: any;
+
+  geneSymbolsMutator(_value, data, _type, _params, _component): string {
+    return data.probeTitles.join(" / ");
+  }
+
+  probeTitlesMutator(_value, data, _type, _params, _component): string {
+    return data.geneSymbols.join(" / ");
+  }
+
+  log2foldMutator(_value, data, _type, _params, _component): number {
+    return data.values[0];
+  }
+
+  pValueMutator(_value, data, _type, _params, _component): number {
+    return data.values[1];
+  }
+
+  columns = [
+    {title: 'Gene Symbol', field: 'type', mutator: this.geneSymbolsMutator},
+    {title: 'Probe Titles', field: 'organism', mutator: this.probeTitlesMutator},
+    {title: 'Probe', field: 'probe'},
+    {title: 'Log2-fold', field: 'log2fold', mutator: this.log2foldMutator},
+    {title: 'P-Value', field: 'pValue', mutator: this.pValueMutator},
+  ]
 
   ngOnInit(): void {
-    this.activatedRoute.queryParams.pipe(
-        filter(params => params.samples)
+    this.activatedRoute.queryParamMap.pipe(
+        filter(paramMap => paramMap.has("samples"))
       ).subscribe(params => {
-        this.samples = params.samples;
+        this.samples = params.getAll("samples");
         this.backend.getMatrix(this.samples)
         .subscribe(
           result => {
-            this.matrix = JSON.stringify(result);
+            console.log(JSON.stringify(result));
+            this.data = result["rows"];
+            this.drawTable();
           }
         )
+    });
+  }
+
+  private drawTable(): void {
+    var _this = this;
+    new Tabulator("#my-tabular-table", {
+      data: this.data,
+      columns: this.columns,
+      layout:"fitDataTable",
+      maxHeight: "75vh",
     });
   }
 
