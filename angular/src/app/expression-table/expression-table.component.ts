ffbfcedfffd5ef1df9cb1ef5e4d0ba15bcb02586
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-02-16 15:46:52 +0900

    Responsively hide "Go to page..." button for small screen sizes

--- angular/src/app/expression-table/expression-table.component.ts
+++ angular/src/app/expression-table/expression-table.component.ts
@@ -129,7 +129,7 @@ export class ExpressionTableComponent implements OnInit, AfterViewInit {
       ],
       tooltips:true,
       ajaxLoaderLoading: "<div class=\"spinner-border text-secondary\" role=\"status\"><span class=\"sr-only\">Loading...</span></div>",
-      footerElement:"<div style=\"float: left;\"><button class=\"tabulator-page\" style=\"border-radius: 4px; border: 1px solid #dee2e6;\" onclick=\"window.dispatchEvent(new CustomEvent(\'OpenModal\'));\">Go to page...</button></div>",
+      footerElement:"<div class=\"d-none d-lg-block\" style=\"float: left;\"><button class=\"tabulator-page\" style=\"border-radius: 4px; border: 1px solid #dee2e6;\" onclick=\"window.dispatchEvent(new CustomEvent(\'OpenModal\'));\">Go to page...</button></div>",
     });
   }
 
