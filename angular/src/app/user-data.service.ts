a84b3cdf4ef5c6524e43374085a2c1c3b122060b
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-06-10 09:49:13 +0900

    Resolve linter errors in user-data.service.ts

--- angular/src/app/user-data.service.ts
+++ angular/src/app/user-data.service.ts
@@ -14,29 +14,29 @@ export class UserDataService {
   static readonly SAMPLE_GROUPS_KEY: string = "sampleGroups_v2";
 
   constructor() {
-    let sampleGroupsJson = window.localStorage.getItem(UserDataService.SAMPLE_GROUPS_KEY);
+    const sampleGroupsJson = window.localStorage.getItem(UserDataService.SAMPLE_GROUPS_KEY);
     if (sampleGroupsJson) {
       this.sampleGroups = new Map(JSON.parse(sampleGroupsJson));
     } else {
-      this.sampleGroups = new Map();
+      this.sampleGroups = new Map<string, ISampleGroup>();
     }
     this.sampleGroupsBehaviorSubject = new BehaviorSubject(this.sampleGroups);
     this.enabledGroupsBehaviorSubject = new BehaviorSubject(this.getEnabledSampleGroups());
   }
 
   private updateSampleGroups() {
-    let sampleGroupsJson = JSON.stringify(Array.from(this.sampleGroups.entries()));
+    const sampleGroupsJson = JSON.stringify(Array.from(this.sampleGroups.entries()));
     window.localStorage.setItem(UserDataService.SAMPLE_GROUPS_KEY, sampleGroupsJson);
     this.sampleGroupsBehaviorSubject.next(this.sampleGroups);
     this.enabledGroupsBehaviorSubject.next(this.getEnabledSampleGroups());
   }
 
-  saveSampleGroups(sampleGroups: Map<string, ISampleGroup>) {
+  saveSampleGroups(sampleGroups: Map<string, ISampleGroup>): void {
     this.sampleGroups = sampleGroups;
     this.updateSampleGroups();
   }
 
-  saveSampleGroup(name: string, samples: string[]) {
+  saveSampleGroup(name: string, samples: string[]): void {
     this.sampleGroups.set(name, <ISampleGroup>{
       name: name,
       samples: samples,
@@ -45,26 +45,26 @@ export class UserDataService {
     this.updateSampleGroups();
   }
 
-  renameSampleGroup(oldName: string, newName: string) {
-    let group = this.sampleGroups.get(oldName);
+  renameSampleGroup(oldName: string, newName: string): void {
+    const group = this.sampleGroups.get(oldName);
     group.name = newName;
     this.sampleGroups.set(newName, group);
     this.sampleGroups.delete(oldName);
     this.updateSampleGroups();
   }
 
-  deleteSampleGroup(name: string) {
+  deleteSampleGroup(name: string): void {
     this.sampleGroups.delete(name);
     this.updateSampleGroups();
   }
 
-  isAcceptableGroupName(name: string) {
+  isAcceptableGroupName(name: string): boolean {
     return !this.sampleGroups.has(name);
   }
 
-  private getEnabledSampleGroups() {
-    let enabledGroups = [];
-    for (let groupName of Array.from(this.sampleGroups.keys()).sort()) {
+  private getEnabledSampleGroups(): ISampleGroup[] {
+    const enabledGroups = new Array<ISampleGroup>();
+    for (const groupName of Array.from(this.sampleGroups.keys()).sort()) {
       if (this.sampleGroups.get(groupName).enabled) {
         enabledGroups.push(this.sampleGroups.get(groupName));
       }
