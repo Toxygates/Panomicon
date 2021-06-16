e1ae4d30bdf49b04e8b8c3cd5901a3a3f9937e47
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-06-16 15:33:39 +0900

    Factor out modal management into sample-filtering component

--- angular/src/app/sample-browser/sample-filtering/sample-filtering.component.spec.ts
+++ angular/src/app/sample-browser/sample-filtering/sample-filtering.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { Subject } from 'rxjs';
 
 import { SampleFilteringComponent } from './sample-filtering.component';
 
-describe('SampleFilterComponent', () => {
+describe('SampleFilteringComponent', () => {
   let component: SampleFilteringComponent;
   let fixture: ComponentFixture<SampleFilteringComponent>;
 
+  class MockModalService {}
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SampleFilteringComponent ]
+      declarations: [ SampleFilteringComponent ],
+      providers: [ { provide: BsModalService, useClass: MockModalService } ]
     })
     .compileComponents();
   });
@@ -16,6 +21,7 @@ describe('SampleFilterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SampleFilteringComponent);
     component = fixture.componentInstance;
+    component.openModalObservable = new Subject<void>();
     component.filters = [];
     fixture.detectChanges();
   });
