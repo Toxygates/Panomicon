9e4fce542093419c23818a2d4963712806361008
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-06-30 13:29:24 +0900

    Move NamedItemStorage to its own file

--- angular/src/app/shared/services/user-data.service.ts
+++ angular/src/app/shared/services/user-data.service.ts
@@ -3,63 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Sample } from '../models/backend-types.model'
 import { IGeneSet, ISampleGroup } from '../models/frontend-types.model'
-
-class NamedItemStorage<T extends {name: string}> {
-
-  private behaviorSubject: BehaviorSubject<Map<string, T>>;
-  private _observable: Observable<Map<string, T>>;
-  public get observable() {
-    return this._observable;
-  }
-
-  constructor(private key: string) {
-    const json = window.localStorage.getItem(key);
-    const parsed = json ? JSON.parse(json) as unknown : [];
-    const array = Array.isArray(parsed) ? parsed : [];
-    this.behaviorSubject = this._observable = new BehaviorSubject(new Map<string, T>(array));
-  }
-
-  currentValue(): Map<string, T> {
-    return this.behaviorSubject.value;
-  }
-
-  updateItems() {
-    const json = JSON.stringify(Array.from(this.behaviorSubject.value));
-    window.localStorage.setItem(this.key, json);
-  }
-
-  setItems(items: Map<string, T>) {
-    this.behaviorSubject.next(items);
-    this.updateItems();
-  }
-
-  setItem(key: string, item: T) {
-    const itemMap = this.behaviorSubject.value;
-    itemMap.set(key, item);
-    this.behaviorSubject.next(itemMap)
-    this.updateItems();
-  }
-
-  deleteItem(key: string) {
-    const itemMap = this.behaviorSubject.value;
-    itemMap.delete(key);
-    this.behaviorSubject.next(itemMap);
-    this.updateItems();
-  }
-
-  renameItem(oldName: string, newName: string): void {
-    const item = this.currentValue().get(oldName);
-    if (!item) throw new Error(`Tried to rename nonexistent item ${oldName}`);
-    item.name = newName;
-    this.setItem(newName, item);
-    this.deleteItem(oldName);
-    this.updateItems();
-  }
-
-  hasItem(key: string) {
-    return this.behaviorSubject.value.has(key);
-  }
-}
+import { NamedItemStorage } from './named-item-storage.class';
 
 @Injectable({
   providedIn: 'root'
