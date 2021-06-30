Toxygates/Panomicon

commit a04ee861a2a93796f0e716b96ec29ed3660e6684

Speed up batch deletion by making use of expected probe sets (platforms) in KCChunkMatrixDB, and by not reading old values before deleting them

OTGTool/src/main/scala/t/db/kyotocabinet/chunk/KCChunkMatrixDB.scala
@@ -239,14 +239,6 @@ class KCChunkMatrixDB(db: DB, writeMode: Boolean)(implicit mc: MatrixContext)
   }
 
   /**
-   * Delete a chunk by key.
-   */
-  private def deleteChunk(sample: Int, probe: Int): Unit = {
-    val key = new VectorChunk[PExprValue](sample, probe, Seq())
-    updateChunk(key)
-  }
-
-  /**
    * Obtain all samples.
    * This operation is slow for the chunk database (all current expression
    * databases).
@@ -267,13 +259,26 @@ class KCChunkMatrixDB(db: DB, writeMode: Boolean)(implicit mc: MatrixContext)
       yield (Sample(c.sample), x._2.copy(probe = probeName))
   }
 
-  private def potentialChunks(x: Sample): Iterable[V] =
-    potentialChunks(x, mc.probeMap.keys.toArray)
+  /**
+   * Construct potential chunks for a sample (empty, not populated).
+   * @param x
+   * @return
+   */
+  private def potentialChunks(x: Sample, probeSets: Iterable[String]): Iterable[V] = {
+    if (probeSets.isEmpty) {
+      potentialChunks(x, mc.probeMap.keysIterator.toArray)
+    } else {
+      for {
+        ps <- probeSets.toArray
+        chunk <- potentialChunks(x, mc.probeSets(ps).toArray)
+      } yield chunk
+    }
+  }
 
   private def potentialChunks(x: Sample, probes: Array[Int]): Iterable[V] = {
     val keys = probes.map(p => chunkStartFor(p)).distinct
     for (k <- keys; dbcode <- x.getDbCode)
-      yield findOrCreateChunk(dbcode, k)
+      yield new VectorChunk[PExprValue](dbcode, k, Seq())
   }
 
   //probes must be sorted in an order consistent with the chunkDB.
@@ -319,19 +324,21 @@ class KCChunkMatrixDB(db: DB, writeMode: Boolean)(implicit mc: MatrixContext)
     r.toArray
   }
 
-  override def deleteSample(s: Sample): Unit = {
-    deleteSamples(List(s))
+  override def deleteSample(s: Sample, probeSets: Iterable[String]): Unit = {
+    deleteSamples(List(s), probeSets)
   }
 
-  override def deleteSamples(ss: Iterable[Sample]): Unit = {
+  override def deleteSamples(ss: Iterable[Sample], probeSets: Iterable[String]): Unit = {
     println(s"Delete samples $ss")
 
     //NB this will not delete chunks if probes/platforms are deleted prior to
     //deleting them, as we only delete probes that are present in the probe map
 
-    for (s <- ss; d <- potentialChunks(s)) {
-      deleteChunk(d.sample, d.start)
-    }
+    val keys = for {
+      s <- ss.toArray; c <- potentialChunks(s, probeSets)
+      key = formKey(c)
+    } yield key
+    db.remove_bulk(keys, false)
   }
 
   override def writeMany(s: Sample, vs: Iterable[(Int, PExprValue)]): Unit = {

OTGTool/src/main/scala/t/db/LookupMap.scala
@@ -29,6 +29,9 @@ trait LookupMap[Code, Token] {
   lazy val keys: Set[Code] = revMap.keySet
   lazy val tokens: Set[Token] = data.keySet
 
+  def keysIterator: Iterator[Code] = revMap.keysIterator
+  def tokensIterator: Iterator[Token] = data.keysIterator
+
   def unpack(item: Code): Token = revMap(item)
   def tryUnpack(item: Code): Option[Token] = revMap.get(item)
 

OTGTool/src/main/scala/t/db/MatrixDB.scala
@@ -181,11 +181,16 @@ trait MatrixDBWriter[E <: ExprValue] {
     }
   }
 
-  def deleteSample(s: Sample): Unit
+  /**
+   * Delete a sample from the specified probe sets.
+   * @param s
+   * @param probeSets Probe sets to delete from. If empty, the sample will be deleted from all probe sets.
+   */
+  def deleteSample(s: Sample, probeSets: Iterable[String]): Unit
 
-  def deleteSamples(ss: Iterable[Sample]): Unit = {
-    for (s <- ss) {
-      deleteSample(s)
+  def deleteSamples(ss: Iterable[Sample], probeSets: Iterable[String]): Unit = {
+    for { s <- ss } {
+      deleteSample(s, probeSets)
     }
   }
 

OTGTool/src/main/scala/t/db/Metadata.scala
@@ -75,7 +75,6 @@ trait Metadata extends SampleSet {
  * Note: the filtering is intended to keep control units intact, so filters should not
  * split such units in two. 
  */
-@deprecated("This class is scheduled for removal", "June 2021")
 class FilteredMetadata(from: Metadata, visibleSamples: Iterable[Sample]) extends Metadata {
   val samples = visibleSamples.toSet
   

OTGTool/src/main/scala/t/db/testing/FakeMatrixDB.scala
@@ -60,5 +60,5 @@ extends AbsFakeMatrixDB[BasicExprValue](initRecords) {
 
   def emptyValue(probe: String) = ExprValue(Double.NaN, 'A', probe)
 
-  def deleteSample(x: Sample): Unit = {}
+  def deleteSample(x: Sample, probeSets: Iterable[String]): Unit = {}
 }

OTGTool/src/main/scala/t/manager/BatchManager.scala
@@ -193,7 +193,7 @@ object BatchManager extends ManagerTool {
       println("Unknown set: " + unknowns)
       if (delete && !unknowns.isEmpty) {
         println("DELETING.")
-        kdb.deleteSamples(unknowns)
+        kdb.deleteSamples(unknowns, List())
       }
     } finally {
       kdb.release
@@ -589,9 +589,9 @@ class BatchManager(context: Context) {
     } yield ()
   }
 
-  private def deleteFromDB(db: MatrixDBWriter[_], samples: Iterable[Sample]) {
+  private def deleteFromDB(db: MatrixDBWriter[_], samples: Iterable[Sample], platforms: Iterable[String]) {
     try {
-      db.deleteSamples(samples)
+      db.deleteSamples(samples, platforms)
     } catch {
       case lf: LookupFailedException =>
         println("Lookup failed for sample, ignoring (possible reason: interrupted data insertion)")
@@ -612,6 +612,7 @@ class BatchManager(context: Context) {
       override def run(): Unit = {
         val bs = new BatchStore(config.triplestoreConfig)
         val ss = bs.getSamples(title).map(Sample(_))
+        val platforms = bs.getPlatforms(title)
         if (ss.isEmpty) {
           log("Nothing to do, batch has no samples")
           return
@@ -624,7 +625,7 @@ class BatchManager(context: Context) {
             chunk <- chunks;
             if shouldContinue(percentComplete)
           ) {
-            deleteFromDB(db, chunk)
+            deleteFromDB(db, chunk, platforms)
             percentComplete += 100 * 25.0 / ss.size
           }
         } finally {
@@ -729,7 +730,7 @@ class BatchManager(context: Context) {
           ++ config.attributes.getUnitLevel.asScala, sf)
 
       val controlGroups = md.treatedControlGroups(md.samples)
-      val treated = controlGroups.toSeq.flatMap(_._1)
+      val treated = controlGroups.flatMap(_._1)
 
       val data = builder.makeNewEmpty(md, treated)
       val total = data.size

OTGTool/src/main/scala/t/sparql/BatchStore.scala
@@ -136,10 +136,17 @@ class BatchStore(config: TriplestoreConfig) extends ListManager[Batch](config) w
     })
   }
 
+  def getPlatforms(batch: String): Iterable[String] = {
+    val prefix = SampleStore.defaultPrefix
+    triplestore.simpleQuery(s"$tPrefixes\nSELECT DISTINCT ?pl WHERE " +
+      s"{ GRAPH <$defaultPrefix/$batch> { ?x a t:sample; t:platform_id ?pl } }")
+
+  }
+
   def getSamples(batch: String): Iterable[SampleId] = {
     val prefix = SampleStore.defaultPrefix
     triplestore.simpleQuery(s"$tPrefixes\nSELECT ?l WHERE " +
-      s"{ graph <$defaultPrefix/$batch> { ?x a t:sample ; rdfs:label ?l } }")
+      s"{ GRAPH <$defaultPrefix/$batch> { ?x a t:sample ; rdfs:label ?l } }")
   }
 
   override def delete(name: String): Unit = {

commit 952f716055c7946cbf024a7957b15b658074ad5a

Fix for KCChunkMatrixDB bug introduced in a04ee861a

OTGTool/src/main/scala/t/db/kyotocabinet/chunk/KCChunkMatrixDB.scala
@@ -264,7 +264,7 @@ class KCChunkMatrixDB(db: DB, writeMode: Boolean)(implicit mc: MatrixContext)
    * @param x
    * @return
    */
-  private def potentialChunks(x: Sample, probeSets: Iterable[String]): Iterable[V] = {
+  private def potentialChunks(x: Sample, probeSets: Iterable[String]): Array[VectorChunk[PExprValue]] = {
     if (probeSets.isEmpty) {
       potentialChunks(x, mc.probeMap.keysIterator.toArray)
     } else {
@@ -275,10 +275,16 @@ class KCChunkMatrixDB(db: DB, writeMode: Boolean)(implicit mc: MatrixContext)
     }
   }
 
-  private def potentialChunks(x: Sample, probes: Array[Int]): Iterable[V] = {
+  private def potentialChunks(x: Sample, probes: Array[Int]): Array[VectorChunk[PExprValue]] = {
     val keys = probes.map(p => chunkStartFor(p)).distinct
     for (k <- keys; dbcode <- x.getDbCode)
       yield new VectorChunk[PExprValue](dbcode, k, Seq())
+  }
+
+  private def findOrCreateChunks(x: Sample, probes: Array[Int]) = {
+    val keys = probes.map(p => chunkStartFor(p)).distinct
+    for (k <- keys; dbcode <- x.getDbCode)
+      yield findOrCreateChunk(dbcode, k)
   }
 
   //probes must be sorted in an order consistent with the chunkDB.
@@ -289,7 +295,7 @@ class KCChunkMatrixDB(db: DB, writeMode: Boolean)(implicit mc: MatrixContext)
 
 //    assert(probes.toSeq.sorted == probes.toSeq)
 
-    val chunks = potentialChunks(x, probes)
+    val chunks = findOrCreateChunks(x, probes)
 
 //    assert(chunks.sortBy(_.start) == chunks)
 //    for (c <- chunks) {