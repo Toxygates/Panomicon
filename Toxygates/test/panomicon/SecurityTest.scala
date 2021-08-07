f719e68f46c9291a5df0d5a8ed9c0bb93f9a8aca
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-08-07 14:12:12 +0900

    Add test for salting and hashing password

--- Toxygates/test/panomicon/SecurityTest.scala
+++ Toxygates/test/panomicon/SecurityTest.scala
@@ -8,7 +8,11 @@ import pdi.jwt.{JwtAlgorithm, JwtClaim, JwtUpickle}
 import t.TTestSuite
 import upickle.default._
 
+import java.security.SecureRandom
 import java.time.Instant
+import java.util.Base64
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
 import scala.util.Failure
 
 @RunWith(classOf[JUnitRunner])
@@ -36,5 +40,31 @@ class SecurityTest extends TTestSuite {
     val token = JwtUpickle.encode(header, claimJson)
     val decode = JwtUpickle.decodeJson(token, "key", Seq(JwtAlgorithm.HS256))
     decode shouldBe a [Failure[ujson.Value]]
+  }
+
+  test("Should be able to get same hashed password after converting salt to and from Base64") {
+    val password = "bad_password"
+
+    val random = new SecureRandom()
+    val originalSalt = new Array[Byte](16)
+    random.nextBytes(originalSalt)
+
+    val spec1 = new PBEKeySpec(password.toCharArray(), originalSalt, 65536, 128)
+    val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+
+    val hash1 = factory.generateSecret(spec1).getEncoded();
+
+    val base64Salt = Base64.getEncoder.encodeToString(originalSalt);
+    println("Salt:")
+    println(base64Salt)
+
+    val decodedSalt = Base64.getDecoder.decode(base64Salt)
+    val spec2 = new PBEKeySpec(password.toCharArray(), decodedSalt, 65536, 128)
+    val hash2 = factory.generateSecret(spec2).getEncoded()
+
+    println("Hashed password:")
+    println(Base64.getEncoder.encodeToString(hash2))
+
+    hash1 should equal(hash2)
   }
 }
