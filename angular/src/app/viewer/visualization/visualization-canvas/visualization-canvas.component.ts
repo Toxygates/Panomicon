6a0b53e971b6eab2f6751bb872ed9ac55585270a
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2022-08-09 14:07:08 +0900

    Display of hello world cytoscape network

--- angular/src/app/viewer/visualization/visualization-canvas/visualization-canvas.component.ts
+++ angular/src/app/viewer/visualization/visualization-canvas/visualization-canvas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as cytoscape from 'cytoscape';
 
 @Component({
   selector: 'app-visualization-canvas',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualization-canvas.component.scss']
 })
 export class VisualizationCanvasComponent implements OnInit {
+	
+	private _cy: any;
 
-  constructor() { }
+  constructor() {	}
 
   ngOnInit(): void {
+		this._cy = cytoscape({
+			container: document.getElementById('cy'),
+			elements: [ // list of graph elements to start with
+			{ // node a
+				data: { id: 'a' }
+			},
+			{ // node b
+				data: { id: 'b' }
+			},
+			{ // edge ab
+				data: { id: 'ab', source: 'a', target: 'b' }
+			}
+		],
+	
+		style: [ // the stylesheet for the graph
+			{
+				selector: 'node',
+				style: {
+					'background-color': '#666',
+					'label': 'data(id)'
+				}
+			},
+	
+			{
+				selector: 'edge',
+				style: {
+					'width': 3,
+					'line-color': '#ccc',
+					'target-arrow-color': '#ccc',
+					'target-arrow-shape': 'triangle',
+					'curve-style': 'bezier'
+				}
+			}
+		],
+	
+		layout: {
+			name: 'grid',
+			rows: 1
+		}
+		}); 
   }
 
 }
