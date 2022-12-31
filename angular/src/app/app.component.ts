8146158aec010dedc0d0fcf28b812434b0d0336f
Yuji Kosugi <yuji.kosugi@gmail.com>
2022-12-31 18:58:31 +0900

    Require login to access viewer (not just admin)

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { BackendService } from './shared/services/backend.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private backend: BackendService,
+  ) { }
 
+  ngOnInit(): void {
+    this.backend.getRoles()
+      .pipe(
+        catchError((error, _caught) => {
+          window.location.href = environment.apiUrl + 'login';
+          throw error;
+        })
+      )
+      .subscribe() // empty subscription to trigger API call
+  }
 }
