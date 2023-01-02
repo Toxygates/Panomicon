74611e0cf2230fcba3ff2374b242d9adc1c607f6
Yuji Kosugi <yuji.kosugi@gmail.com>
2023-01-02 11:06:34 +0900

    Only show admin link to admin users

--- angular/src/app/viewer/viewer.component.ts
+++ angular/src/app/viewer/viewer.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { SampleGroup } from '../shared/models/frontend-types.model';
 import { UserDataService } from '../shared/services/user-data.service';
 import { environment } from 'src/environments/environment';
+import { FetchedDataService } from '../shared/services/fetched-data.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-viewer',
@@ -11,12 +13,18 @@ import { environment } from 'src/environments/environment';
 })
 export class ViewerComponent implements OnInit {
 
-  constructor(private userData: UserDataService) {}
+  constructor(
+    private userData: UserDataService,
+    private fetcheddata: FetchedDataService,
+  ) {}
 
   navbarIsCollapsed = true;
   enabledSampleGroupsExist = false;
 
   enabledGroups$!: Observable<SampleGroup[]>;
+  showAdminLink$ = this.fetcheddata.roles$.pipe(
+    map(roles => roles?.includes('admin'))
+  );
 
   ngOnInit(): void {
     this.enabledGroups$ = this.userData.enabledGroups$;
