e19a58401066ccc1b55d337fea3ec5cd35e08fc9
Yuji Kosugi <yuji.kosugi@gmail.com>
2023-01-03 17:54:32 +0900

    Add UI to pick sample groups and gene set for network

--- angular/src/app/viewer/visualization/network-display/network-display.component.ts
+++ angular/src/app/viewer/visualization/network-display/network-display.component.ts
@@ -1,4 +1,8 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GeneSet, SampleGroup } from 'src/app/shared/models/frontend-types.model';
+import { UserDataService } from 'src/app/shared/services/user-data.service';
 import { DisplayCanvasComponent } from '../display-canvas/display-canvas.component';
 import { LayoutPickerComponent } from '../layout-picker/layout-picker.component';
 import { Network } from './network';
@@ -18,9 +22,39 @@ export class NetworkDisplayComponent implements AfterViewInit {
 
 	@ViewChild('networkCanvas')
 	networkCanvas!: DisplayCanvasComponent;
+
+	firstNetworkSampleGroupOptions$!: Observable<SampleGroup[]>;
+	secondNetworkSampleGroupOptions$!: Observable<SampleGroup[]>;
+	geneSets$!: Observable<GeneSet[]>;
+
+	firstNetworkSampleGroup$!: BehaviorSubject<string | null>;
+	secondNetworkSampleGroup$!: BehaviorSubject<string | null>;
+	networkGeneSet$!: BehaviorSubject<string | null>;
 	
-	constructor() { 
+	constructor(
+		private userData: UserDataService,
+	) {
 		this.network = new Network();
+		this.firstNetworkSampleGroupOptions$ = this.userData.sampleGroups$.pipe(
+			map(sampleGroups => {
+				return [...sampleGroups.values()]
+					.filter(group => group.type === 'mRNA');
+			})
+		);
+		this.secondNetworkSampleGroupOptions$ = this.userData.sampleGroups$.pipe(
+			map(sampleGroups => {
+				return [...sampleGroups.values()]
+					.filter(group => group.type === 'miRNA');
+			})
+		);
+
+		this.geneSets$ = userData.geneSets$.pipe(
+			map(geneSets => geneSets ? [...geneSets.values()] : [])
+		);
+
+		this.firstNetworkSampleGroup$ = userData.firstNetworkSampleGroupName$;
+		this.secondNetworkSampleGroup$ = userData.secondNetworkSampleGroupName$;
+		this.networkGeneSet$ = userData.networkGeneSetName$;
 	}
 	
 	ngAfterViewInit(): void {
--- angular/src/app/viewer/visualization/visualization.module.ts
+++ angular/src/app/viewer/visualization/visualization.module.ts
@@ -5,12 +5,16 @@ import { SharedModule } from '../../shared/shared.module';
 import { NetworkDisplayComponent } from './network-display/network-display.component';
 import { LayoutPickerComponent } from './layout-picker/layout-picker.component';
 import { DisplayCanvasComponent } from './display-canvas/display-canvas.component';
+import { SampleGroupPickerComponent } from './sample-group-picker/sample-group-picker.component';
+import { GeneSetPickerComponent } from './gene-set-picker/gene-set-picker.component';
 
 @NgModule({
   declarations: [
     NetworkDisplayComponent,
     LayoutPickerComponent,
-    DisplayCanvasComponent
+    DisplayCanvasComponent,
+    SampleGroupPickerComponent,
+    GeneSetPickerComponent,
   ],
   imports: [
     CommonModule,
