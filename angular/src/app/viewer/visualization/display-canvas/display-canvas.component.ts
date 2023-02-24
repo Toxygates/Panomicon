772fd9175e3199c06a301545f18d704c5eff21b2
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-02-24 12:52:25 +0900

    Add color and shape to node display.

--- angular/src/app/viewer/visualization/display-canvas/display-canvas.component.ts
+++ angular/src/app/viewer/visualization/display-canvas/display-canvas.component.ts
@@ -1,6 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Network } from 'src/app/shared/models/backend-types.model';
-import cytoscape, { EdgeDataDefinition, NodeDataDefinition } from 'cytoscape';
+import cytoscape, {
+  EdgeDataDefinition,
+  NodeDataDefinition,
+  Stylesheet,
+} from 'cytoscape';
+
+/** default colours for nodes in the graph */
+const nodeColor = Object.freeze({
+  mRNA: '#007f7f',
+  microRNA: '#827f00',
+  HIGHLIGHT: '#ffde4c',
+  SELECTED: '#00de4c',
+  CONNECTED: '#6fde95',
+});
+
+/** list of shapes that can be used to draw a node */
+const nodeShape = Object.freeze({
+  mRNA: 'ellipse',
+  microRNA: 'pentagon',
+});
 
 @Component({
   selector: 'app-display-canvas',
@@ -46,6 +65,8 @@ export class DisplayCanvasComponent implements OnInit {
           type: n.type,
           symbols: n.symbols,
           weights: n.weights,
+          color: n.type === 'mRNA' ? nodeColor.mRNA : nodeColor.microRNA,
+          shape: n.type === 'mRNA' ? nodeShape.mRNA : nodeShape.microRNA,
         } as NodeDataDefinition,
       } as cytoscape.ElementDefinition;
     });
@@ -75,27 +96,8 @@ export class DisplayCanvasComponent implements OnInit {
   ngOnInit(): void {
     this._cy = cytoscape({
       container: document.getElementById('cy'),
-      style: [
-        {
-          selector: 'node',
-          style: {
-            label: 'data(label)',
-            'text-valign': 'center',
-            'text-halign': 'center',
-            'background-color': 'data(color)',
-            'border-color': 'data(borderColor)',
-            'border-width': '1px',
-            display: 'element',
-          },
-        },
-        {
-          selector: 'edge',
-          style: {
-            'line-color': 'data(color)',
-          },
-        },
-      ],
     } as cytoscape.CytoscapeOptions);
+    this.initStyle();
   }
 
   public updateLayout(name = '', boundingBox = undefined): void {
@@ -105,15 +107,29 @@ export class DisplayCanvasComponent implements OnInit {
       padding: 0, // fit padding
       boundingBox: boundingBox, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
       animate: false, // whether to transition the node positions
-      animationDuration: 500, // duration of animation in ms if enabled
-      animationEasing: undefined, // easing of animation if enabled
-      // animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-      ready: undefined, // callback on layoutready
-      // stop: function() { window.removePendingRequest(); }, // callback on layoutstop
-      // transform: function (node, position ){ return position; }, // transform a given node position. Useful for changing flow direction in discrete layouts
-      // weaver: weaver
     } as cytoscape.LayoutOptions);
     layout.run();
     this._cy.fit();
+  }
+
+  public initStyle(): void {
+    if (this._cy.container() === null) return;
+
+    const nodeStyle: Stylesheet = {
+      selector: 'node',
+      css: {
+        label: 'data(label)',
+        'text-valign': 'center',
+        'text-halign': 'center',
+        shape:
+          'data(shape)' as cytoscape.Css.PropertyValueNode<cytoscape.Css.NodeShape>,
+        'background-color': 'data(color)',
+        'border-color': 'data(borderColor)',
+        'border-width': '1px',
+        display: 'element',
+      },
+    };
+
+    this._cy.style([nodeStyle]);
   }
 }
